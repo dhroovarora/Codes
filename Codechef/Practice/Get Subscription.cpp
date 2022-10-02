37b05cb8e5c07600e81d01371dc302c0466133aa
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-02 06:23:19 +0530

    Create Get Subscription.cpp

---
+++ Codechef/Practice/Get Subscription.cpp
@@ -0,0 +1,24 @@
+https://www.codechef.com/problems/SUBSCRIBE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int f;
+	cin>>f;
+	while(f--){
+	    int dhroov;
+	    cin>>dhroov;
+	    if(dhroov<=30){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
