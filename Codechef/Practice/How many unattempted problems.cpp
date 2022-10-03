d7d52b910202e86d7777296b002c32b4bae33c40
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-03 20:01:44 +0530

    Create How many unattempted problems.cpp

---
+++ Codechef/Practice/How many unattempted problems.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/PRACLIST
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	cout<<x-y;
+	return 0;
+}
