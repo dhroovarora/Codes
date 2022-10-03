92f74bcfae69b65a21e65199454aeb2af9527797
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-03 20:00:34 +0530

    Create Who is taller!.cpp

---
+++ Codechef/Practice/Who is taller!.cpp
@@ -0,0 +1,24 @@
+https://www.codechef.com/problems/TALLER
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b){
+	        cout<<'A'<<endl;
+	    }
+	    else{
+	        cout<<'B'<<endl;
+	    }
+	}
+	    return 0;
+}
