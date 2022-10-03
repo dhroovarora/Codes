7d74d26ffb47c7cb7968b13444eeb71f8bae1d16
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-03 20:06:30 +0530

    Create Sleep deprivation.cpp

---
+++ Codechef/Practice/Sleep deprivation.cpp
@@ -0,0 +1,22 @@
+https://www.codechef.com/problems/SLEEP
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<7)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
