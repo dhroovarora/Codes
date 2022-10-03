7bb3dc62c135b3d31d6799b920c1bf23d6092ba3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-03 19:59:46 +0530

    Create My very 1st contest.cpp

---
+++ Codechef/Practice/My very 1st contest.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/MY1STCONTEST
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin>>n>>a>>b;
+	cout<<n-a<<" "<<n-a-b;
+	return 0;
+}
