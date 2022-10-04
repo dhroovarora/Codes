4cb32df921ecca07abe3b780432600b7612ac13e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-04 08:15:50 +0530

    Create Ranklist Pages.cpp

---
+++ Codechef/Practice/Ranklist Pages.cpp
@@ -0,0 +1,22 @@
+https://www.codechef.com/problems/RANKLISTPAGE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%25)
+	        cout<<x/25 + 1<<endl;
+	    else
+	        cout<<x/25<<endl;
+	}
+	return 0;
+}
