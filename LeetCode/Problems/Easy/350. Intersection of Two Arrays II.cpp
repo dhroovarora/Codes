d0ec5b2b0137f16e1cd521eefa3948d9b1d5d0c9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-05 09:12:10 +0530

    Create 350. Intersection of Two Arrays II.cpp

---
+++ LeetCode/Problems/Easy/350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums1.size();i++){
+            mp[nums1[i]]++;
+        }
+        vector<int> v;
+        for(int i=0;i<nums2.size();i++){
+            if(mp[nums2[i]] > 0){
+                mp[nums2[i]]--;
+                v.push_back(nums2[i]);
+            }
+        }
+        return v;
+    }
+};
