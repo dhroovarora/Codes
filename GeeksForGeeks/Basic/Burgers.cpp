43388f2a7679cd233e6f1297d7d8e5b08ada3306
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-07 23:53:09 +0530

    Create Burgers.cpp

---
+++ GeeksForGeeks/Basic/Burgers.cpp
@@ -0,0 +1,23 @@
+https://www.codechef.com/problems/BURGERS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<b)
+	        cout<<a<<endl;
+	    else
+	        cout<<b<<endl;
+	}
+	return 0;
+}
