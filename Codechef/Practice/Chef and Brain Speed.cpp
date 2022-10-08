67c6e67129cd1e355548bcff9d096ae4659431a3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-08 22:19:34 +0530

    Create Chef and Brain Speed.cpp

---
+++ Codechef/Practice/Chef and Brain Speed.cpp
@@ -0,0 +1,18 @@
+https://www.codechef.com/problems/CBSPEED
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(y <= x)
+	    cout<<"NO";
+	else
+	    cout<<"YES";
+	return 0;
+}
