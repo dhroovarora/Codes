f1aefab99340480cc51c1162a664a4f6911ad169
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-10 22:34:10 +0530

    Create Podium Finish.cpp

---
+++ Codechef/Practice/Podium Finish.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/PODIUM
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	}
+	return 0;
+}
