48b916ed78bfc39ee7ff4651b041cc9f37d3210f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-10 10:43:00 +0530

    Create 1748. Sum of Unique Elements.cpp

---
+++ LeetCode/Problems/Easy/1748. Sum of Unique Elements.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/sum-of-unique-elements/
+
+
+
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        int sum = 0;
+        for(auto i :m){
+            if(i.second ==1){
+                sum += i.first;
+            }
+        }
+        return sum;
+    }
+};
