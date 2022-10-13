72999bf0b39f21b36a145d692a9f12c8520af2ad
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-13 23:07:26 +0530

    Create Number of Credits.cpp

---
+++ Codechef/Practice/Number of Credits.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CREDS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    cout<<x*4 + y*2<<endl;
+	}
+	return 0;
+}
