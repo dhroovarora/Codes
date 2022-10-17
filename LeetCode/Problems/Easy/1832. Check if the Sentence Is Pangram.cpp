8b5a176f826483fd4755381746eb9b2e49301e7d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-10-17 22:12:28 +0530

    Create 1832. Check if the Sentence Is Pangram.cpp

---
+++ LeetCode/Problems/Easy/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+
+
+
+class Solution {
+public:
+     bool checkIfPangram(string sentence) {
+        set<int> s;
+        for (auto& c: sentence)
+            s.insert(c);
+        return s.size() == 26;
+    }
+};
