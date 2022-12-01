cb11bacc528856fd2376a06d4603d5efb2ea75b1
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-12-01 11:41:56 +0530

    Update 908. Smallest Range I.cpp

--- LeetCode/Problems/Easy/908. Smallest Range I.cpp
+++ LeetCode/Problems/Easy/908. Smallest Range I.cpp
@@ -3,3 +3,9 @@ https://leetcode.com/problems/smallest-range-i/
 
 
 
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        return max(0, *max_element(begin(A), end(A)) - 2*K - *min_element(begin(A), end(A)));
+    }
+};
