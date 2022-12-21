a398312f987698b266a7946c3ea5407c6fc43159
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-12-21 17:29:06 +0530

    Create Water Consumption.cpp

---
+++ Codechef/Practice/Water Consumption.cpp
@@ -0,0 +1,22 @@
+https://www.codechef.com/problems/WATERCONS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x >= 2000)
+	        cout<<"YES"<<endl;
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
