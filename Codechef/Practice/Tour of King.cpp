98fb1975694562d2919105a14f513e2909640e7d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-12-27 13:59:22 +0530

    Create Tour of King.cpp

---
+++ Codechef/Practice/Tour of King.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/KNGTOR
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m;
+	    cin >> n >> m;
+	    cout << 5*n + 7*m << endl;
+	}
+	return 0;
+}
