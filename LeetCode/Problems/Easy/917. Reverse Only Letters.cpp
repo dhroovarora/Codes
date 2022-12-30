84068e5971ac1bbca692b268d989e9f92684d240
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-12-30 18:06:30 +0530

    Create 917. Reverse Only Letters.cpp

---
+++ LeetCode/Problems/Easy/917. Reverse Only Letters.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/reverse-only-letters/description/
+
+
+
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')){
+                if((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z')){
+                    swap(s[i],s[j]);
+                    i++;
+                    j--;
+            }
+                else
+                    j--;
+            }
+            else
+                i++;
+        }
+    return s;
+    }
+};
