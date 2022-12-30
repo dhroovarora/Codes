d63643ebc4ebdf800027ba87c97a1478415f1ba0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2022-12-30 13:26:09 +0530

    Create Waiting Time.cpp

---
+++ Codechef/Practice/Waiting Time.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/WAITTIME
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int k,x;
+	    cin >> k >> x;
+	    cout << 7*k - x << endl;
+	}
+	return 0;
+}
