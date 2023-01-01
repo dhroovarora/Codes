6e5344d25e289e54237d29ec38c9e6e3a65ddab4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-02 01:13:44 +0530

    Create Non-Negative Product.cpp

---
+++ Codechef/Contests/START57C/Non-Negative Product.cpp
@@ -0,0 +1,34 @@
+https://www.codechef.com/problems/NONNEGPROD
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    int count = 0;
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        if(a[i] == 0){
+	            count = 0;
+	            break;
+	        }
+	        else if(a[i] < 0){
+	            count++;
+	        }
+	    }
+	    cout<<count%2<<endl;
+	    
+	}
+	return 0;
+}
