4ad51e32321d09ed3a9d6fb9e8b910720b9a8414
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-03 11:12:42 +0530

    Create Chef and Chapters.cpp

---
+++ Codechef/Practice/Chef and Chapters.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/SEMCOURSES
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y,z;
+	    cin >> x >> y >> z;
+	    cout << x*y*z << endl;
+	}
+	return 0;
+}
