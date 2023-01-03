fc1d77083824f93233a9f7f77d1c03b55816be9b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-03 11:25:14 +0530

    Create Apples and Oranges.cpp

---
+++ Codechef/Practice/Apples and Oranges.cpp
@@ -0,0 +1,18 @@
+https://www.codechef.com/problems/APPLORNG
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,a,b;
+	cin >> x >> a >> b;
+	if(x >= a + b)
+	    cout << "YES" << endl;
+	else
+	    cout << "NO"<<endl;
+	return 0;
+}
