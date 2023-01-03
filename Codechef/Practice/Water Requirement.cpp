4a1996798339dca090b8f8de7537304c6c391447
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-03 11:14:14 +0530

    Create Water Requirement.cpp

---
+++ Codechef/Practice/Water Requirement.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/WATERREQ
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	     cout << 2*n << endl;
+	}
+	return 0;
+}
