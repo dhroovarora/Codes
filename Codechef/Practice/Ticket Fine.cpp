a2db521919e9d9b3f88807f5a55fd6f87667b8df
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-05 13:48:21 +0530

    Create Ticket Fine.cpp

---
+++ Codechef/Practice/Ticket Fine.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/TCKTFINE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,p,q;
+	    cin >> x >> p >> q;
+	    cout << x * (p-q) << endl;
+	}
+	return 0;
+}
