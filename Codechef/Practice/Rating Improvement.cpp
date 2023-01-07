7e12d36850a1e09e3d36090fb829f6a89f068f27
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-07 18:21:18 +0530

    Create Rating Improvement.cpp

---
+++ Codechef/Practice/Rating Improvement.cpp
@@ -0,0 +1,22 @@
+https://www.codechef.com/problems/ADVANCE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    if(y >= x && y <= x + 200)
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}
