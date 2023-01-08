6a972e15c50e92df24bc6cdd7c4d4db5faa0792c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-08 16:51:31 +0530

    Create Discount.cpp

---
+++ Codechef/Practice/Discount.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/DISCNT
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    cout << 100 - x << endl;
+	}
+	return 0;
+}
