2dea447f21dca86d4b3637d27dfbf4372be6ec91
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-09 15:37:00 +0530

    Create Fill the Bucket.cpp

---
+++ Codechef/Practice/Fill the Bucket.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/FBC
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int k,z;
+	    cin >> k >> z;
+	    cout << k - z << endl;
+	}
+	return 0;
+}
