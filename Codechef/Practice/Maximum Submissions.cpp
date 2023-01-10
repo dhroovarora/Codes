c10004fbe2fcb23a8d783534531013783d81dcfb
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-10 15:49:22 +0530

    Create Maximum Submissions.cpp

---
+++ Codechef/Practice/Maximum Submissions.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/MAXIMUMSUBS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    cout << (x*60-5) / 30  + 1<< endl;
+	}
+	return 0;
+}
