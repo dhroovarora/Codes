b084ce46512ba7af00de13b29d0253c9b88f7edb
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-10 15:53:46 +0530

    Create AI Analysing Code.cpp

---
+++ Codechef/Practice/AI Analysing Code.cpp
@@ -0,0 +1,18 @@
+https://www.codechef.com/problems/AIANALYSE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int c;
+	cin >> c;
+	if(c <= 1000)
+	    cout << "Yes" << endl;
+	 else
+	    cout << "No" << endl;
+	return 0;
+}
