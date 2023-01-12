71aa8fa368b5b2d2dcfdd35e702d5aa805f09b2c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-12 13:51:33 +0530

    Create Tyre problem.cpp

---
+++ Codechef/Practice/Tyre problem.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/TYRE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m;
+	    cin >> n >> m;
+	    cout << 2*n + 4*m << endl;
+	}
+	return 0;
+}
