64a19c427ec2da43ede61ea13647dcf2f88586d2
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-12 14:42:12 +0530

    Create Chef and Instant Noodles.cpp

---
+++ Codechef/Practice/Chef and Instant Noodles.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/INSTNOODLE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin >> x >> y;
+	cout << x*y << endl;
+	return 0;
+}
