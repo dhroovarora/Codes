d33474c67daa90260cba764ff910eb9d70f2a8df
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-13 18:55:37 +0530

    Create Credit score.cpp

---
+++ Codechef/Practice/Credit score.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/CREDSCORE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin >> x;
+	(x >= 750) ? cout << "Yes" : cout << "No";
+	return 0;
+}
