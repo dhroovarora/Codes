0b66e286027017aa37af2a5439ad6b490ffcd761
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-17 22:21:29 +0530

    Create Chef and Chocolates2.cpp

---
+++ Codechef/Practice/Chef and Chocolates2.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CHEFCHOCO
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int c,x,y;
+	    cin >> c >> x >>y;
+	    cout << (c-x)*y << endl;
+	}
+	return 0;
+}
