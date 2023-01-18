abec714f62266c76cdb84533af443f9fc0d2c537
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-18 18:27:12 +0530

    Create Janmansh and Assignments.cpp

---
+++ Codechef/Practice/Janmansh and Assignments.cpp
@@ -0,0 +1,20 @@
+https://www.codechef.com/problems/JASSIGNMENTS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    (x>7) ? cout << "No" : cout <<"Yes";
+	    cout << endl;
+	}
+	return 0;
+}
