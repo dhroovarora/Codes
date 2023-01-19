8351ec389baf8b5e81acd267acdf4d38a1365e27
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-19 20:54:42 +0530

    Create CRED Coins.cpp

---
+++ Codechef/Practice/CRED Coins.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CREDCOINS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    cout << (x*y) / 100 << endl;
+	}
+	return 0;
+}
