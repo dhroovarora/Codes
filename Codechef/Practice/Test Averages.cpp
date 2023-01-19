d7e50184f80c4d182b1800214946e3286b518e06
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-20 00:30:32 +0530

    Create Test Averages.cpp

---
+++ Codechef/Practice/Test Averages.cpp
@@ -0,0 +1,20 @@
+https://www.codechef.com/problems/TESTAVG
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b,c;
+	    cin >> a >> b >> c;
+	    ((a+b)/2 < 35 || (b+c)/2 < 35 || (a+c)/2 < 35) ? cout << "FAIL" : cout << "PASS";
+	    cout << endl;
+	}
+	return 0;
+}
