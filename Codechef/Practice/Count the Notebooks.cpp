d03fd77ce4d025183cf8ac62fb3039a249058bdc
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-21 09:44:24 +0530

    Create Count the Notebooks.cpp

---
+++ Codechef/Practice/Count the Notebooks.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/NOTEBOOK
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << n*10 << endl;
+	}
+	return 0;
+}
