d6ce053483d4cdffa979a533627a3da33e7bee5b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-21 09:42:45 +0530

    Create Sugarcane Juice Business.cpp

---
+++ Codechef/Practice/Sugarcane Juice Business.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/SUGARCANE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << 15*n << endl;
+	}
+	return 0;
+}
