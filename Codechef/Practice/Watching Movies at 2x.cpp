c5ed5d41a53e06da33a1b970fccf2574e10ee92e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-24 01:42:05 +0530

    Create Watching Movies at 2x.cpp

---
+++ Codechef/Practice/Watching Movies at 2x.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/MOVIE2X
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin >> x >> y;
+	cout << x - y/2 << endl;
+	return 0;
+}
