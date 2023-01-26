a5cfc5cb4abc581f77bc87283cdcf38bb045077c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-26 19:09:19 +0530

    Create Bath in Winters.cpp

---
+++ Codechef/Practice/Bath in Winters.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/BATH
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    cout << x/(2*y) << endl;
+	}
+	return 0;
+}
