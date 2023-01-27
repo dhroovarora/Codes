a33e767486c44d350f4f8b52e20ec65b1b6012f3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-27 15:49:53 +0530

    Create Profit Increment.cpp

---
+++ Codechef/Practice/Profit Increment.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/PROINC
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    cout << x/10 + y << endl;
+	}
+	return 0;
+}
