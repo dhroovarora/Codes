6463cb012cfa1922731de92354b49e7486217425
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-01-30 12:52:23 +0530

    Create Valentine is Coming.cpp

---
+++ Codechef/Practice/Valentine is Coming.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/VALENTINE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    cout << x/y << endl;
+	}
+	return 0;
+}
