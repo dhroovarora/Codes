80a4dd57ebd04e3c0def59f456f9025fa51a14ea
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-02 23:19:20 +0530

    Create Black cells in a chessboard.cpp

---
+++ Codechef/Practice/Black cells in a chessboard.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/BLACKCEL
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	cout << (n/2)*n << endl;
+	return 0;
+}
