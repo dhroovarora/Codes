b1bd366d3e03ea585e1e982e8464ef6658698fa6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-07 23:10:29 +0530

    Create Helping Chef.cpp

---
+++ Codechef/Practice/Helping Chef.cpp
@@ -0,0 +1,20 @@
+https://www.codechef.com/problems/FLOW008
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    (n >= 10) ? cout << -1 : cout << "Thanks for helping Chef!";
+	    cout << endl;
+	}
+	return 0;
+}
