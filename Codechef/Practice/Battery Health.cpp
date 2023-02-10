8baaa6fd7f62089669f178960fac1af97980c914
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-10 16:05:56 +0530

    Create Battery Health.cpp

---
+++ Codechef/Practice/Battery Health.cpp
@@ -0,0 +1,20 @@
+https://www.codechef.com/problems/BTRYHLTH
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    (x >= 80) ? cout << "YES" : cout << "NO";
+	    cout << endl;
+	}
+	return 0;
+}
