0abaf959a91fd163640eb7147b1802b3d8f7066e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-10 16:16:14 +0530

    Create Economics Class.cpp

---
+++ Codechef/Practice/Economics Class.cpp
@@ -0,0 +1,29 @@
+https://www.codechef.com/problems/ECOCLASS
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int s[n],d[n];
+	    int ans = 0;
+	    for(int i=0;i<n;i++)
+	        cin >> s[i];
+	    for(int i=0;i<n;i++)
+	        cin >> d[i];
+	    for(int i=0;i<n;i++){
+	        if(s[i] == d[i])
+	            ans ++;
+	    }
+	    cout << ans << endl;
+	}
+	return 0;
+}
