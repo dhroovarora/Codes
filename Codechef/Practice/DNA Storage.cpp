930d12e0bb2708a71ec4309db600caee9b2aa808
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-15 10:10:44 +0530

    Create DNA Storage.cpp

---
+++ Codechef/Practice/DNA Storage.cpp
@@ -0,0 +1,32 @@
+https://www.codechef.com/problems/DNASTORAGE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    string s;
+	    cin >> s;
+	    string ans = "";
+	    for(int i=0;i<n;i=i+2){
+	        if(s[i] == '0' && s[i+1] == '0')
+	            ans += 'A';
+	        else if(s[i] == '0' && s[i+1] == '1')
+	            ans += 'T';
+	        else if(s[i] == '1' && s[i+1] == '0')
+	            ans += 'C';
+	        else
+	            ans += 'G';
+	    }
+	    cout << ans << endl;
+	}
+	return 0;
+}
