690da648fc5c3aae25afc00259306c2477761569
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-02-27 11:15:12 +0530

    Create Coldplay.cpp

---
+++ Codechef/Practice/Coldplay.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/SLOOP
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int m,s;
+	    cin >> m >> s;
+	    cout << m/s << endl;
+	}
+	return 0;
+}
