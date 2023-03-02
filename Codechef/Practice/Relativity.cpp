bdc49ce8d6ff3b57fe7a53af67d65cc8ebcd309a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-02 19:14:20 +0530

    Create Relativity.cpp

---
+++ Codechef/Practice/Relativity.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/RELATIVE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int g,c;
+	    cin >> g >> c;
+	    cout << (c*c)/(2*g) << endl;
+	}
+	return 0;
+}
