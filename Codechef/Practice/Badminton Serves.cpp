28eac9bd07b4a6c98ce5c3b043f1741df37e57f4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-02 19:02:03 +0530

    Create Badminton Serves.cpp

---
+++ Codechef/Practice/Badminton Serves.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/BADMINTON
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int p;
+	    cin >> p;
+	    cout << p/2 + 1 << endl;
+	}
+	return 0;
+}
