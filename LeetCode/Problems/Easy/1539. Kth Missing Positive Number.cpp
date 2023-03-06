b4f70a515c7e2c18c548258c7ea73cc97d925d1d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-06 22:55:34 +0530

    Create 1539. Kth Missing Positive Number.cpp

---
+++ LeetCode/Problems/Easy/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/kth-missing-positive-number/description/
+
+
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int start = 0, end = n-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - (mid + 1) < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start + k;
+    }
+};
