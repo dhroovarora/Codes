6bcd1c33ba331c276ef70ef5973a2e62348643c3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-11 10:15:03 +0530

    Create Chess Match.cpp

---
+++ Codechef/Practice/Chess Match.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/BLITZ3_2
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,a,b;
+	    cin >> n >> a >> b;
+	    cout << 2*(180 + n) - (a+b) << endl;
+	}
+	return 0;
+}
