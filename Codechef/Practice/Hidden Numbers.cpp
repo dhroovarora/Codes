720a7caced7d663e2197ab972f5b5bb2f0ea2ffa
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-11 09:58:15 +0530

    Create Hidden Numbers.cpp

---
+++ Codechef/Practice/Hidden Numbers.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/UWCOI21A
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << 1 << " " << n << endl;
+	}
+	return 0;
+}
