0d26261cd37726da160a0e1a954c0ab1a57fa7c9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-21 17:46:29 +0530

    Create The Begining Era Of Cyberverse.cpp

---
+++ Codechef/Practice/The Begining Era Of Cyberverse.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CYBV
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,k;
+	    cin >> n >> k;
+	    cout << k/n << endl;
+	}
+	return 0;
+}
