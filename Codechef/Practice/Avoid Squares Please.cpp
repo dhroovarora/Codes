4e300ee23b475362df49436abdc02a1dd9244c82
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-22 16:48:14 +0530

    Create Avoid Squares Please.cpp

---
+++ Codechef/Practice/Avoid Squares Please.cpp
@@ -0,0 +1,21 @@
+https://www.codechef.com/problems/AVDSQUARPLZ
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    for(int i=1;i<=n;i++)
+	        cout << i << " ";
+	    cout << endl;
+	}
+	return 0;
+}
