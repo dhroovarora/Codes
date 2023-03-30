ad2473830b7aa8d1e5151615b83b17e4f1ed2a79
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-30 11:53:32 +0530

    Create Budget of Technex.cpp

---
+++ Codechef/Practice/Budget of Technex.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/BUDTECH
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int r;
+	    cin >> r;
+	    cout << r*100 << endl;
+	}
+	return 0;
+}
