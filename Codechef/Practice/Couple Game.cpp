90740283bfc67444b303d9ef7a989e42bea311cc
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-03-30 11:51:50 +0530

    Create Couple Game.cpp

---
+++ Codechef/Practice/Couple Game.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/COUGAME
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int g,b;
+	    cin >> g >> b;
+	    cout << b - g << endl;
+	}
+	return 0;
+}
