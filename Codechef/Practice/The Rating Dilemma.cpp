dc2458476e2d59aa95f924f5139b70eed32e3fff
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-02 17:56:59 +0530

    Create The Rating Dilemma.cpp

---
+++ Codechef/Practice/The Rating Dilemma.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/RATING
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << (-1)*(n+1) << endl;
+	}
+	return 0;
+}
