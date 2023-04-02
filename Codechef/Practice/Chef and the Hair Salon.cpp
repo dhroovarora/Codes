06de19e259bc85f3a9bd284ac7f165b52ccb84eb
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-02 13:25:05 +0530

    Create Chef and the Hair Salon.cpp

---
+++ Codechef/Practice/Chef and the Hair Salon.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CHEFBARBER
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m;
+	    cin >> n >> m;
+	    cout << n * m << endl;
+	}
+	return 0;
+}
