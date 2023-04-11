b7f59813c87d55b344826f725f1100ee6e6664ba
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-12 01:29:21 +0530

    Create 1037. Valid Boomerang.cpp

---
+++ LeetCode/Problems/Easy/1037. Valid Boomerang.cpp
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/valid-boomerang/description/
+
+
+
+
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& p){
+        return (p[0][0] * (p[1][1] - p[2][1]) + p[1][0] * (p[2][1] - p[0][1]) + p[2][0] * (p[0][1] - p[1][1]));
+    }
+};
