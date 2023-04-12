88306ae832aa20e5277d22d2afcf944d46c3bf8e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-12 18:58:14 +0530

    Create 1450. Number of Students Doing Homework at a Given Time.cpp

---
+++ LeetCode/Problems/Easy/1450. Number of Students Doing Homework at a Given Time.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/description/
+
+
+
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int ans = 0;
+        for(int i=0;i<startTime.size();i++){
+            if(queryTime >= startTime[i] && queryTime <= endTime[i])
+                ans++;
+        }
+        return ans;
+    }
+};
