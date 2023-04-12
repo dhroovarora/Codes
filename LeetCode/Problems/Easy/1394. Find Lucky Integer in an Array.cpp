451ffcedd11e50e0678aaa230cd4b03afa0fd78b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-12 16:39:03 +0530

    Create 1394. Find Lucky Integer in an Array.cpp

---
+++ LeetCode/Problems/Easy/1394. Find Lucky Integer in an Array.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/find-lucky-integer-in-an-array/description/
+
+
+
+
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int,int> m;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]]++;
+        }
+        int ans = -1;
+        for(auto i:m){
+            if(i.first == i.second)
+                ans = max(ans,i.first);
+        }
+        return ans;
+    }
+};
