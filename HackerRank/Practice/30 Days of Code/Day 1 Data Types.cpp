a515b4764655de9913c227249efa4d85514be7b3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 15:08:36 +0530

    Create Day 1 Data Types.cpp

---
+++ HackerRank/Practice/30 Days of Code/Day 1 Data Types.cpp
@@ -0,0 +1,26 @@
+https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+
+
+
+
+
+
+    #include<string>
+    // Declare second integer, double, and String variables.
+    int a;
+    double b;
+    string c;
+    // Read and save an integer, double, and String to your variables.
+    cin>>a>>b;
+    cin.ignore();
+    getline(cin, c);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    cout<< a + i<<endl;
+    // Print the sum of the double variables on a new line.
+    cout<<fixed;
+    cout<<setprecision(1)<<b + d<<endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout<< s << c;
