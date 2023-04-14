05820c69f4563214cd2475c71612d988ed30a71f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 14:16:22 +0530

    Create Weather Observation Station 6.sql

---
+++ HackerRank/Practice/SQL/Weather Observation Station 6.sql
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+
+
+
+/*
+Enter your query here.
+*/
+select CITY from STATION where left(CITY,1) IN ("A","E","I","O","U");
