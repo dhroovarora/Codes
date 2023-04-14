4786f56074eb0fa060bf96783d9c2f7f2489c010
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 14:13:35 +0530

    Create Weather Observation Station 3.sql

---
+++ HackerRank/Practice/SQL/Weather Observation Station 3.sql
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+
+
+
+
+/*
+Enter your query here.
+*/
+select DISTINCT CITY from STATION where ID % 2 = 0;
