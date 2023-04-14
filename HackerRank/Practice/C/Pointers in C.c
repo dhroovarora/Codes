6dcae9ed6d92789deffdfea0989644b708ca385b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 15:50:24 +0530

    Create Pointers in C.c

---
+++ HackerRank/Practice/C/Pointers in C.c
@@ -0,0 +1,27 @@
+https://www.hackerrank.com/challenges/pointer-in-c/problem?isFullScreen=true
+
+
+
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+    int temp = *a;
+    *a = *a + *b;
+    *b = *b - temp;
+    if(*b <0){
+        *b = *b * (-1);
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
