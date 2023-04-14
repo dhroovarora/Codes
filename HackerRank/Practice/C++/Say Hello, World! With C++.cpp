d8f645bb178190c215d57db16ca687cdc3a01daa
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 14:29:20 +0530

    Create Say Hello, World! With C++.cpp

---
+++ HackerRank/Practice/C++/Say Hello, World! With C++.cpp
@@ -0,0 +1,13 @@
+https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
