1ef2a2b2ab1d2bc6328034dfea1f70dbb2b751a6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-14 14:14:07 +0530

    Create Weather Observation Station 4.sql

---
+++ HackerRank/Practice/SQL/Weather Observation Station 4.sql
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+
+
+
+
+/*
+Enter your query here.
+*/
+select Count(CITY) - Count(DISTINCT CITY) from STATION;
