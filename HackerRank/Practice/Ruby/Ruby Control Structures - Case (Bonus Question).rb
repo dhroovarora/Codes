559bce9aeea12aa247d2ac3113dbbf4db924c1ee
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-16 23:08:50 +0530

    Create Ruby Control Structures - Case (Bonus Question).rb

---
+++ HackerRank/Practice/Ruby/Ruby Control Structures - Case (Bonus Question).rb
@@ -0,0 +1,26 @@
+https://www.hackerrank.com/challenges/ruby-case/problem?isFullScreen=true
+
+
+
+
+
+def identify_class(obj)
+    # write your case control structure here
+    case obj
+        when Hacker
+        puts  "It's a Hacker!"
+        
+        when Submission
+        puts "It's a Submission!"
+        
+        when TestCase
+        puts "It's a TestCase!"
+        
+        when Contest
+        puts "It's a Contest!"
+        
+        else
+        puts "It's an unknown model"
+        
+    end
+end
