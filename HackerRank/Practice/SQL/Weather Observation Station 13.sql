1b8ab62cb4d45c047959489bb5f66533973f2eb6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-16 19:20:29 +0530

    Create Weather Observation Station 13.sql

---
+++ HackerRank/Practice/SQL/Weather Observation Station 13.sql
@@ -0,0 +1,6 @@
+https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
+
+
+
+
+select round(sum(lat_n),4) from station where lat_n > 38.7880 and lat_n < 137.2345;
