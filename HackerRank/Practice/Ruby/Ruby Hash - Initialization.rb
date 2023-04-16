462fa838ba999fe2c6553ef05bc6351fac5a0e01
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-16 23:39:05 +0530

    Create Ruby Hash - Initialization.rb

---
+++ HackerRank/Practice/Ruby/Ruby Hash - Initialization.rb
@@ -0,0 +1,10 @@
+https://www.hackerrank.com/challenges/ruby-hash-initialization/problem?isFullScreen=true
+
+
+
+
+# Initialize 3 variables here as explained in the problem statement
+empty_hash = Hash.new
+default_hash = Hash.new(1)
+hackerrank = Hash.new
+hackerrank = {"simmy" => 100 , "vivmbbs" => 200}
