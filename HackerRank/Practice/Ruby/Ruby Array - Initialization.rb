ee33906c0fbb1cf503b1d59c885746f063a87b5c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-16 23:13:56 +0530

    Create Ruby Array - Initialization.rb

---
+++ HackerRank/Practice/Ruby/Ruby Array - Initialization.rb
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/ruby-array-initialization/problem?isFullScreen=true
+
+
+
+
+# Initialize 3 variables here as explained in the problem statement
+array = []
+array_1 = Array.new(1)
+array_2 = [10,10]
