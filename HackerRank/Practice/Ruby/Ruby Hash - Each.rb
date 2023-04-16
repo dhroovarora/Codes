37973ad0c684eaa0c752d3dcf5ea40155c913f13
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-17 01:01:44 +0530

    Create Ruby Hash - Each.rb

---
+++ HackerRank/Practice/Ruby/Ruby Hash - Each.rb
@@ -0,0 +1,12 @@
+https://www.hackerrank.com/challenges/ruby-hash-method-each/problem?isFullScreen=true
+
+
+
+
+def iter_hash(hash)
+    # your code here
+    hash.each do |key,value|
+        puts key
+        puts value
+    end
+end
