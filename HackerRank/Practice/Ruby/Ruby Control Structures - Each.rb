4fdbb529141475b7fa9213de15e93730ce22c9d5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-16 22:57:39 +0530

    Create Ruby Control Structures - Each.rb

---
+++ HackerRank/Practice/Ruby/Ruby Control Structures - Each.rb
@@ -0,0 +1,11 @@
+https://www.hackerrank.com/challenges/ruby-tutorial-each/problem?isFullScreen=true
+
+
+
+
+def scoring(array)
+    # iterate through each of the element in array using *each* and call update_score on it
+    array.each do |user|
+        user.update_score
+    end
+end
