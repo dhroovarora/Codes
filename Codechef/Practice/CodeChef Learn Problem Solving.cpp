8370dad2ecdce032e91b0d4a9f90ae08f37a6fa8
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-19 23:55:46 +0530

    Create CodeChef Learn Problem Solving.cpp

---
+++ Codechef/Practice/CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/CCLEARN
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	cout << n*2 << endl;
+	return 0;
+}
