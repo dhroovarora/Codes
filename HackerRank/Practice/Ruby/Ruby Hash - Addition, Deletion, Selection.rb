f52422fbe2b015f844ead6e59a220c89ca34b527
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-22 15:12:38 +0530

    Create Ruby Hash - Addition, Deletion, Selection.rb

---
+++ HackerRank/Practice/Ruby/Ruby Hash - Addition, Deletion, Selection.rb
@@ -0,0 +1,8 @@
+https://www.hackerrank.com/challenges/ruby-hash-add-del-sel/problem?isFullScreen=true
+
+
+
+
+hackerrank.store(543121,100)
+hackerrank.keep_if {|key,value| key.is_a?Integer}
+hackerrank.delete_if {|key,value| key%2 == 0}
