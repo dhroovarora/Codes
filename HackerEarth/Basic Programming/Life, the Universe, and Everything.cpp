21721e967d21c4a14ae70f0383b92dd1ce0fcca1
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-22 14:36:28 +0530

    Create Life, the Universe, and Everything.cpp

---
+++ HackerEarth/Basic Programming/Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
+
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	while(true){
+		int n;
+		cin >> n;
+		if(n==42)
+			break;
+		cout << n << endl;
+	}
+}
