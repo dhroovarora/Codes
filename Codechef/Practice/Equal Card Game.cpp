ed015ce9b5452c1ac91e829440a2cb4bd54292ec
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-24 11:08:42 +0530

    Create Equal Card Game.cpp

---
+++ Codechef/Practice/Equal Card Game.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/EQCARDGAME
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int k;
+	    cin >> k;
+	    cout << 52%k << endl;
+	}
+	return 0;
+}
