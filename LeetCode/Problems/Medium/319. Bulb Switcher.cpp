987fb5c3bae0ad82b8791f2b9da1ab61887a3dd1
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-27 22:35:45 +0530

    Create 319. Bulb Switcher.cpp

---
+++ LeetCode/Problems/Medium/319. Bulb Switcher.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/bulb-switcher/description/
+
+
+
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // int ans = 0;
+        // int i=1;
+        // if(n==0)
+        //     return 0;
+        // while(i<=n){
+        //     int j = sqrt(i);
+        //     if(j * j == i)
+        //         ans++;
+        //     i++;         
+        // }
+        // return ans;
+        
+        return sqrt(n);
+    }
+};
