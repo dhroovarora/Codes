965fe8756dda951a1362043d91d4d62bea7ca92a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-04-28 13:35:44 +0530

    Create Ruby - Enumerable - Introduction.rb

---
+++ HackerRank/Practice/Ruby/Ruby - Enumerable - Introduction.rb
@@ -0,0 +1,9 @@
+https://www.hackerrank.com/challenges/ruby-enumerable-introduction/problem?isFullScreen=true
+
+
+
+
+def iterate_colors(colors)
+  # Your code here
+    colors.map{|item| item}
+end
