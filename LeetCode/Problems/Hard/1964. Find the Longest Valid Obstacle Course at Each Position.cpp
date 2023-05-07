d90f04f433155a26f7612fc710a9cddd8fc8fc19
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-05-07 10:56:19 +0530

    Create 1964. Find the Longest Valid Obstacle Course at Each Position.cpp

---
+++ LeetCode/Problems/Hard/1964. Find the Longest Valid Obstacle Course at Each Position.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
+
+
+
+
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        vector<int> v(obstacles.size()+1, INT_MAX), ans;
+
+        for(auto it : obstacles)   {
+            int ind = upper_bound(v.begin(), v.end(), it) - v.begin();
+            v[ind] = it;
+            ans.push_back(ind+1);
+        }
+
+        return ans; 
+    }
+};
