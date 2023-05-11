0430ee447e064afd9143468b8eb228aa35c724ea
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-05-11 11:19:24 +0530

    Create Mean and Median.cpp

---
+++ Codechef/Contests/START55C/Mean and Median.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/START55C/problems/MEANMEDIAN
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<-500<<" "<<y<<" "<<3*x-y  + 500<<endl;
+	}
+	return 0;
+}
