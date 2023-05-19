7da485698bad57317e90905203afdb0f3d3b1d4e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-05-19 15:54:58 +0530

    Create Learning SQL.cpp

---
+++ Codechef/Practice/Learning SQL.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/LEARNSQL
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int r,c,e;
+	cin >> r >> c >> e;
+	cout << (r+e)*c;
+	return 0;
+}
