c6c40a45e6194955e1a60f6224c7c573f518fcb4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-06-04 23:30:03 +0530

    Create Lucky Boundaries.cpp

---
+++ Codechef/Practice/Lucky Boundaries.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/ICM0013
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << n << endl;
+	}
+	return 0;
+}
