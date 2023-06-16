e754ed77913badb8ea9e66a7106b7b1416e045e3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-06-16 23:22:15 +0530

    Create Akash and Function.cpp

---
+++ Codechef/Practice/Akash and Function.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CHFDBT
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << (n+1)/2 << endl;
+	}
+	return 0;
+}
