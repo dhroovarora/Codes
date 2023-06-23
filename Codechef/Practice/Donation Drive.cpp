cd416f97d8ba823207ff48f0c083fa6e25411e9a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-06-23 18:31:25 +0530

    Create Donation Drive.cpp

---
+++ Codechef/Practice/Donation Drive.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/DONDRIVE
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,x;
+	    cin >> n >> x;
+	    cout << n - x << endl;
+	}
+	return 0;
+}
