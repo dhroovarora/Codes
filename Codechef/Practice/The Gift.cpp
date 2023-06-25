9501581d13e29494738366ee8b76737f77f790cd
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-06-25 23:29:00 +0530

    Create The Gift.cpp

---
+++ Codechef/Practice/The Gift.cpp
@@ -0,0 +1,16 @@
+https://www.codechef.com/problems/CS2023_GIFT
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,n,m;
+	cin >> x >> n >> m;
+	(x+m >= n) ? cout << "YES" : cout << "NO";
+	cout << endl;
+	return 0;
+}
