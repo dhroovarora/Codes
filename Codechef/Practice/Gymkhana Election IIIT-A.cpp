bff731b8823bafe3b949035ceff17409760cbb24
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-06-27 20:54:41 +0530

    Create Gymkhana Election IIIT-A.cpp

---
+++ Codechef/Practice/Gymkhana Election IIIT-A.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/problems/CS2023_GYM
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,m;
+	    cin >> n >> m;
+	    cout << m/2 + 1 << endl;
+	}
+	return 0;
+}
