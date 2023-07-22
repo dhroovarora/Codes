b7fe6871c2b2ceed764f4084e849914b83acd504
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-07-22 20:30:34 +0530

    Create Puzzle Hunt.cpp

---
+++ Codechef/Practice/Puzzle Hunt.cpp
@@ -0,0 +1,15 @@
+https://www.codechef.com/problems/PUZHUNT
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	(n >=6 && n<= 8) ? cout << "YES" : cout << "NO";
+	return 0;
+}
