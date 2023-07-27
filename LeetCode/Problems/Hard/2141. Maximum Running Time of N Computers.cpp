f9026c00fe78d64989329ba29ecab5857a376530
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-07-27 20:51:12 +0530

    Create 2141. Maximum Running Time of N Computers.cpp

---
+++ LeetCode/Problems/Hard/2141. Maximum Running Time of N Computers.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/maximum-running-time-of-n-computers/description/
+
+
+
+
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& b) {
+        int m = b.size();
+        sort(b.begin(),b.end());
+        long long sum = accumulate(begin(b),end(b),0LL);
+        int i=m-1;
+        while(i>=0 && sum/n < b[i] ){
+            sum -= b[i];
+            n--;i--;
+        }
+        return sum/n;
+    }
+};
