ac0295aa0b2690338ec2d2df1548618d04714b11
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-08-11 22:12:06 +0530

    Create 518. Coin Change II.cpp

---
+++ LeetCode/Problems/Medium/518. Coin Change II.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/coin-change-ii/description/
+
+
+
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1,0);
+        dp[0] = 1;
+        for(auto i : coins){
+            for(int j = i; j<= amount;j++){
+                dp[j] += dp[j-i];
+            }
+        }
+        return dp[amount];
+    }
+};
