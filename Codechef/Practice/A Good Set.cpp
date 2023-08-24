06d58f4c70380aa66972d2796bf40229731d36c9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-08-24 23:43:55 +0530

    Create A Good Set.cpp

---
+++ Codechef/Practice/A Good Set.cpp
@@ -0,0 +1,21 @@
+https://www.codechef.com/problems/GOODSET
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    for(int i=1;i<2*n;i+=2)
+	        cout << i << " ";
+	    cout << endl;
+	}
+	return 0;
+}
