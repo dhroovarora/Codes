46230fe94775eaf593f20c5960286bb5e4f77403
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-09-19 10:24:16 +0530

    Create Biryani classes.cpp

---
+++ Codechef/Practice/Biryani classes.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/PCPP10/problems/CPPDEBUG02
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++) {
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x*y<<endl;
+	}
+	return 0;
+}
