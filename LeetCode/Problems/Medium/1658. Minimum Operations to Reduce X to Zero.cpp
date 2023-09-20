d6581ee129248910a08cdae640e2c071a305fa7d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-09-20 07:30:50 +0530

    Create 1658. Minimum Operations to Reduce X to Zero.cpp

---
+++ LeetCode/Problems/Medium/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/description/?envType=daily-question&envId=2023-09-20
+
+
+
+
+class Solution {
+public:
+    int minOperations(std::vector<int>& nums, int x) {
+        int total = std::accumulate(nums.begin(), nums.end(), 0);
+        int target = total - x;
+        int left = 0;
+        int n = nums.size();
+        int maxLength = -1;
+        int runningSum = 0;
+
+        for (int right = 0; right < n; right++) {
+            runningSum += nums[right];
+
+            while (runningSum > target && left <= right) {
+                runningSum -= nums[left];
+                left++;
+            }
+
+            if (runningSum == target) {
+                maxLength = std::max(maxLength, right - left + 1);
+            }
+        }
+
+        return maxLength != -1 ? n - maxLength : -1;
+    }
+};
