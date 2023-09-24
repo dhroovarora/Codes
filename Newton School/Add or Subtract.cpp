34285bec7f318142d81dc125b12f403b3e4e60c3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-09-24 10:46:35 +0530

    Create Add or Subtract.cpp

---
+++ Newton School/Add or Subtract.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/xb80zvvw9b0g
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int x,y;
+    cin >> x >> y;
+    (y%x) ? cout << y - x : cout << x + y;
+    return 0;
+}
