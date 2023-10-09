898156a58a1d47f7d7e84588eea57a83944360b7
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-09 14:00:28 +0530

    Create Even Index.cpp

---
+++ Newton School/Even Index.cpp
@@ -0,0 +1,20 @@
+https://my.newtonschool.co/playground/code/969117kz4cc5
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    for(int i=0;i<n;++i){
+        int a;
+        cin >> a;
+        if(i%2==0)
+            cout << a << " ";
+    }
+    return 0;
+}
