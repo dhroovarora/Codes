41dc39bd3fa5d5134c0df02b64de60a8aede69c9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-19 13:26:09 +0530

    Create Fitness.cpp

---
+++ Codechef/Practice/Fitness.cpp
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/PCPPBM01/problems/FIT
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    cout << 10*x << endl;
+	}
+	return 0;
+}
