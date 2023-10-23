299aa53fa6a7bd7aa650c97ca723aa648c7eb5c3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-23 13:06:34 +0530

    Update 342. Power of Four.cpp

--- LeetCode/Problems/Easy/342. Power of Four.cpp
+++ LeetCode/Problems/Easy/342. Power of Four.cpp
@@ -3,22 +3,29 @@ https://leetcode.com/problems/power-of-four/
 
 
 
+// class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         else{
+//             if(n%4==0){
+//                 return isPowerOfFour(n/4);
+//             }
+//             else{
+//                 return 0;
+//             }
+//         }
+//     }
+// };
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        else{
-            if(n%4==0){
-                return isPowerOfFour(n/4);
-            }
-            else{
-                return 0;
-            }
-        }
+        int mask = 0x55555555;
+        return n > 0 && (n & (n - 1)) == 0 && (n & mask) == n;
     }
 };
