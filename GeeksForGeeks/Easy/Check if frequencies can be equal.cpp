9e66e186db7ed6fd9d2361336525e563fa38ead6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-25 21:52:24 +0530

    Create Check if frequencies can be equal.cpp

---
+++ GeeksForGeeks/Easy/Check if frequencies can be equal.cpp
@@ -0,0 +1,34 @@
+https://practice.geeksforgeeks.org/problems/check-frequencies4211/1?page=5&difficulty[]=-1&difficulty[]=0&status[]=unsolved&sortBy=submissions
+
+
+
+
+//User function template for C++
+class Solution{
+public:	
+	 bool sameFreq(string s)
+    {
+        // code here 
+        unordered_map<char,int> um;
+        for(int i=0;i<s.size();i++)
+        {
+            um[s[i]]++;
+            
+        }
+        vector<int> v;
+        for(auto x:um)
+        {
+            v.push_back(x.second);
+        }
+        sort(v.begin(),v.end());
+        
+        if(v[v.size()-1]-v[0]==0)
+            return(1);
+        if((v[v.size()-1]-v[0]==1 )&& (v[v.size()-1]!=v[v.size()-2]))
+            return(1);
+        if(v[0]==1 && v[1]==v[v.size()-1])
+            return(1);
+        
+        return(0);
+    }
+};
