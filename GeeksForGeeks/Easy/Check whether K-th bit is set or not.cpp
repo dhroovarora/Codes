420cb685b8f9ad4f908f4dc2471fcdc407cc2f18
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-29 22:33:56 +0530

    Create Check whether K-th bit is set or not.cpp

---
+++ GeeksForGeeks/Easy/Check whether K-th bit is set or not.cpp
@@ -0,0 +1,16 @@
+https://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+
+
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        return n&(1<<k);
+    }
+};
