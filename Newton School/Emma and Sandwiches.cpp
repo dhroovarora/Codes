37e96d25b499b8a36e6f168875b2b64bfe512f59
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-10-31 22:32:12 +0530

    Create Emma and Sandwiches.cpp

---
+++ Newton School/Emma and Sandwiches.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/9qpr4c2exvj1
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n,m;
+    cin >> n >> m;
+    cout << min(n/2,m);
+    return 0;
+}
