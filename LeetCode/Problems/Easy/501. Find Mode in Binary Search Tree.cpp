b3a7f88b336a6b1348b5120ccbbab4b0082eb6ed
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-11-01 15:09:43 +0530

    Create 501. Find Mode in Binary Search Tree.cpp

---
+++ LeetCode/Problems/Easy/501. Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,46 @@
+https://leetcode.com/problems/find-mode-in-binary-search-tree/?envType=daily-question&envId=2023-11-01
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int val = INT_MIN, freq = 0, maxFreq = 0;
+    vector<int> ans;
+
+    void inOrder(TreeNode* Node) {
+        if (Node == NULL) return;
+        inOrder(Node->left);
+        if (val==Node->val) 
+            freq++;
+        else {
+            val=Node->val;// update val
+            freq=1;
+        }
+        if (freq>maxFreq) {
+            maxFreq=freq;
+            ans={Node->val};
+        } 
+        else if (freq==maxFreq) 
+            ans.push_back(Node->val);
+        
+        inOrder(Node->right);
+       
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        inOrder(root);
+        return ans;
+    }
+};
