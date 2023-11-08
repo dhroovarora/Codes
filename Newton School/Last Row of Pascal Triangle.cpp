8dd5f5f88b975791e08beae5a8a5593b1c4610f0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-11-08 06:39:51 +0530

    Create Last Row of Pascal Triangle.cpp

---
+++ Newton School/Last Row of Pascal Triangle.cpp
@@ -0,0 +1,17 @@
+https://my.newtonschool.co/playground/code/judzvn0b0dio
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    while(n--){
+        cout << 1 <<' ';
+    }
+    return 0;
+}
