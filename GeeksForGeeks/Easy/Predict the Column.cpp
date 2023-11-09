f1b5741c0a4d709bb06239f03bd79e4ea0731fb9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-11-09 14:23:00 +0530

    Create Predict the Column.cpp

---
+++ GeeksForGeeks/Easy/Predict the Column.cpp
@@ -0,0 +1,34 @@
+https://www.geeksforgeeks.org/problems/predict-the-column/1
+
+
+
+
+class Solution{
+    public:
+    /*Function to count zeros in each column
+    * N : Number of rows and columns in array
+    M is the matrix that is globally declared
+    */
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+        // Your code here
+        int count =0, colume=-1;
+        for(int i=0;i<N;i++)
+        {
+            int curr=0;
+            for(int j=0;j<N;j++)
+            {
+                if(arr[j][i]==0)
+                {
+                    curr++;
+                }
+            }
+            if(curr>count)
+            {
+                count=curr;
+                colume=i;
+            }
+        }
+        return colume;
+    }
+};
