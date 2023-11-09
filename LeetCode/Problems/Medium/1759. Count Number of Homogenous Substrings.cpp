8ae511a3957e570623dba08f2f1cffa6c52ca44e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-11-09 14:22:32 +0530

    Create 1759. Count Number of Homogenous Substrings.cpp

---
+++ LeetCode/Problems/Medium/1759. Count Number of Homogenous Substrings.cpp
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/count-number-of-homogenous-substrings/description/?envType=daily-question&envId=2023-11-09
+
+
+
+
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int M=1e9+7;
+        int ans=0;
+        int curr_cnt=1;
+        ans+=1;
+        for(int i=1;i<s.length();i++){
+            if(s[i]!=s[i-1]){
+                curr_cnt=1;
+                ans+=curr_cnt;
+            }
+            else{
+                curr_cnt++;
+                ans+=curr_cnt;
+                ans=ans%M;
+            }
+        }
+        return ans;
+    }
+};
