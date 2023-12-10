e528a174d57b562b3eee4198ff6b92eb2ee5e838
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-10 22:01:48 +0530

    Create 867. Transpose Matrix.cpp

---
+++ LeetCode/Problems/Easy/867. Transpose Matrix.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/transpose-matrix/?envType=daily-question&envId=2023-12-10
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        vector<vector<int>> res;
+
+        for (size_t c = 0; c < matrix[0].size(); c++) {
+            vector<int> temp;
+
+            for (size_t r = 0; r < matrix.size(); r++) {
+                temp.push_back(matrix[r][c]);
+            }
+
+            res.push_back(temp);
+        }
+
+        return res;        
+    }
+};
