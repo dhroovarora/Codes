4a8570ed5879c96a2aed57511ae6abbba92fcb0d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-16 17:42:27 +0530

    Create Aman and Puzzle.cpp

---
+++ Newton School/Aman and Puzzle.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/ibm5ur4v0asc
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int x,y,z;
+    cin >> x >> y >> z;
+    cout << (x/y + x/z)/2;
+    return 0;
+}
