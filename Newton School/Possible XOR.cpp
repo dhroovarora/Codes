1c33206811e53e84f37a66828b0b320c78eb9c40
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-18 00:39:05 +0530

    Create Possible XOR.cpp

---
+++ Newton School/Possible XOR.cpp
@@ -0,0 +1,18 @@
+https://my.newtonschool.co/playground/code/w7dgj1a9dvsm
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    if(n%2)
+        cout << "NO";
+    else
+        cout << "YES";
+    return 0;
+}
