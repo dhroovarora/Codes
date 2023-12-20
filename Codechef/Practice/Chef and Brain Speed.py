cacda39cb20907d9d4119bbeb0a66c31b0e0e189
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-20 21:28:59 +0530

    Create Chef and Brain Speed.py

---
+++ Codechef/Practice/Chef and Brain Speed.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/practice/course/python/PPY03/problems/CBSPEED?tab=statement
+
+
+
+
+# cook your dish here
+a,b = map(int,input().split())
+if a < b:
+    print("YES")
+else:
+    print("NO")
