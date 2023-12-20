56f6cfefbfd8eaf86c9da66fe687269fec5927ed
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-20 21:34:07 +0530

    Create Minimum Choices.cpp

---
+++ Newton School/Minimum Choices.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/t8ztmki0dods
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    cout << n + 1;
+    return 0;
+}
