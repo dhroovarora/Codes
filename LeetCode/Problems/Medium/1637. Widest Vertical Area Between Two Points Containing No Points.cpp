36876c3dc7e2bf64b7ec7ca07297a534e5f29b8f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-21 12:55:25 +0530

    Create 1637. Widest Vertical Area Between Two Points Containing No Points.cpp

---
+++ LeetCode/Problems/Medium/1637. Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/?envType=daily-question&envId=2023-12-21
+
+
+
+
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(),points.end());
+        int diff =0;
+        for(int i =1;i<points.size();i++){
+            diff = max(diff,points[i][0]-points[i-1][0]);
+        }
+        return diff;
+    }
+};
