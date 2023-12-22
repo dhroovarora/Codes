957b141632cab11585f850c0c77b34f29b62af87
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-22 11:34:50 +0530

    Create Alice and Marks.py

---
+++ Codechef/Practice/Alice and Marks.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/practice/course/python/PPY03/problems/MARKSTW
+
+
+
+
+# cook your dish here
+x,y = map(int,input().split())
+if x >= 2*y:
+    print("Yes")
+else:
+    print("No")
