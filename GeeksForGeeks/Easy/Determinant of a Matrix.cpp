7414af992fff7c378902932f6633e189b62867b6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-25 22:51:46 +0530

    Create Determinant of a Matrix.cpp

---
+++ GeeksForGeeks/Easy/Determinant of a Matrix.cpp
@@ -0,0 +1,34 @@
+https://www.geeksforgeeks.org/problems/determinant-of-a-matrix-1587115620/1
+
+
+
+
+class Solution
+{   
+    public:
+    //Function for finding determinant of matrix.
+    int determinantOfMatrix(vector<vector<int> > matrix, int n)
+    {
+        // code here 
+          if(n==1)return matrix[0][0];
+        int m = 0;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            vector<vector<int>>InnerMat(n-1, vector<int>(n-1));
+            int p = 0;
+            for(int j=1; j<n; j++){
+                int q = 0;
+                for(int k=0; k<n; k++){
+                    if(k==i){
+                        continue;
+                    }
+                    InnerMat[p][q++] = matrix[j][k];
+                }
+                p++;
+            }
+            if(i%2==0)ans+=(matrix[m][i])*(determinantOfMatrix(InnerMat,n-1));
+            else ans-=(matrix[m][i])*(determinantOfMatrix(InnerMat,n-1));
+        }
+        return ans;
+    }
+};
