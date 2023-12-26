df96515779e051248de12d41483db9bf7ae8aef9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-26 23:41:50 +0530

    Create Add Two Numbers.py

---
+++ Codechef/Practice/Add Two Numbers.py
@@ -0,0 +1,10 @@
+https://www.codechef.com/practice/course/python/PPY05/problems/FLOW001
+
+
+
+
+t = int(input())
+for i in range(0,t):
+    a,b = map(int,input().split())
+    # write your code here
+    print(a+b)
