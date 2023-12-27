b69b3400c549fc398926f380b1e9cd25f277e695
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-27 22:53:11 +0530

    Create N Sliice of Pizza.cpp

---
+++ Newton School/N Sliice of Pizza.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/1shm578502t3
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    (n%2 && n != 1) ? cout << "No" : cout << "Yes";
+    return 0;
+}
