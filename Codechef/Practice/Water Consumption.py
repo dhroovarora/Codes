71b033891d55a709f2362d85b1450adec7b7ffbe
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-30 22:31:29 +0530

    Create Water Consumption.py

---
+++ Codechef/Practice/Water Consumption.py
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/python/PPY05/problems/WATERCONS
+
+
+
+
+t = int(input())
+for i in range(0,t):
+    x =  int(input())
+    # write your code here
+    if(x >= 2000):
+        print("YES")
+    else:
+        print("NO")
