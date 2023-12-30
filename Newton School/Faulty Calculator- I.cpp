7aa311347c6c06ae0f6c134fc83b2a49d717a295
Dhroov <dhroov1864.be20@chitkara.edu.in>
2023-12-30 22:47:19 +0530

    Create Faulty Calculator- I.cpp

---
+++ Newton School/Faulty Calculator- I.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/6su7pza6nos3
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a,b;
+    cin >> a >> b;
+    cout << (a+b)*10 + 3;
+    return 0;
+}
