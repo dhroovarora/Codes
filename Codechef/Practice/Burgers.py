51e920c811f9215587bbeb56a34822c4738a307d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-02 19:35:17 +0530

    Create Burgers.py

---
+++ Codechef/Practice/Burgers.py
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/python/PPY10/problems/PYDEBUG03
+
+
+
+
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b=map(int,input(). split (' '))
+    if a<=b:
+        print(a)
+    else: 
+        print(b)
