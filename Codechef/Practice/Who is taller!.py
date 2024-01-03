6776c5a098bf4b3a6268ee9952a02c53faf7794f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-03 20:44:35 +0530

    Create Who is taller!.py

---
+++ Codechef/Practice/Who is taller!.py
@@ -0,0 +1,12 @@
+https://www.codechef.com/practice/course/python/PPY10/problems/PYDEBUG04
+
+
+
+
+# cook your dish here
+for _ in range(int(input())):
+    A,B=map(int,input().split())
+    if A>B:
+        print('A')
+    else:
+        print('B')
