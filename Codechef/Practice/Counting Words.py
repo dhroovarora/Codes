65a6e95a01defb0d56ec44eafc3761ffb1e0af00
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-08 20:50:45 +0530

    Create Counting Words.py

---
+++ Codechef/Practice/Counting Words.py
@@ -0,0 +1,10 @@
+https://www.codechef.com/practice/course/python/PPY10/problems/PYDEBUG10
+
+
+
+
+# cook your dish here
+T=int(input())
+for _ in range(T):
+    n,m=map(int,input().split())
+    print(n*m)
