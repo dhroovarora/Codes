371a62713824eaaa5ab5fca109149b2734bd8501
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-09 20:53:55 +0530

    Create Data Types.py

---
+++ Codechef/Practice/Data Types.py
@@ -0,0 +1,18 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP01_V2/problems/GS02
+
+
+
+
+# Click 'Submit' to view the result
+
+a = -5          # Integer variable
+print(a)
+
+b = 4.0         # Float variable
+print(b)
+
+s = "1234abc"   # String variable
+print(s)
+
+array = [1, "ab", 3, "name"]    # Array variable
+print(array)
