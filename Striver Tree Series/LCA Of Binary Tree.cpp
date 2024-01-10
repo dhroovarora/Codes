1f0480e611940b39f2b6d5e934c36ea63242e5f9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-11 03:17:39 +0530

    Create LCA Of Binary Tree.cpp

---
+++ Striver Tree Series/LCA Of Binary Tree.cpp
@@ -0,0 +1,42 @@
+https://www.codingninjas.com/studio/problems/lca-of-binary-tree_920541
+
+
+
+
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int lowestCommonAncestor(TreeNode<int> *root, int x, int y) {
+  //    Write your code here
+    if(!root)
+        return -1;
+    if(root->data == x || root->data == y)
+        return root->data;
+    
+    int l = lowestCommonAncestor(root->left, x , y);
+    int r = lowestCommonAncestor(root->right, x , y);
+
+    if(l == -1)
+        return r;
+    else if(r == -1)
+        return l;
+    else
+        return root->data;
+}
