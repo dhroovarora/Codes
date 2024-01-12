ca9692340e34943310941461a3e26522bdfec6a5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-12 21:36:48 +0530

    Create How to accept multiple inputs in a line.py

---
+++ Codechef/Practice/How to accept multiple inputs in a line.py
@@ -0,0 +1,9 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP01_V2/problems/GS06
+
+
+
+
+# Replace the underscores with the correct value
+
+A, B, C = map(int,input().split())
+print(A, B, C)
