92b97811130556ac97e9038105ec3ade9c67dbca
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-13 22:42:01 +0530

    Create How to accept multiple integers on separate lines.py

---
+++ Codechef/Practice/How to accept multiple integers on separate lines.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP01_V2/problems/GS07
+
+
+
+
+# Update the '_' in the code below
+
+A, B = map(int,input().split())
+C, D, E = map(int, input().split())
+F, G, H, I = map(int,input().split())
+print(A, B, C, D, E, F, G, H, I)
