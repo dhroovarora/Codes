bf9805e9dd2c40983badd9d19c05d646640d9099
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-14 18:17:33 +0530

    Create How to accept string inputs.py

---
+++ Codechef/Practice/How to accept string inputs.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP01_V2/problems/GS08
+
+
+
+
+# Update the '_' in the code below
+
+A, B = input().split()
+C, D, E = input().split()
+F, G, H, I = input().split()
+print(A, B, C, D, E, F, G, H, I)
