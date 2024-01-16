a21982e35999a6bd404038894255c2d69be409ce
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-16 12:39:09 +0530

    Create Test cases.py

---
+++ Codechef/Practice/Test cases.py
@@ -0,0 +1,17 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP02_V2/problems/GS09A
+
+
+
+
+# Update the '_' in the code below
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
