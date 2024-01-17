6719f4a9c8d6c583661642c817d9023e1a5e0713
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-18 00:54:18 +0530

    Create Number of strings.cpp

---
+++ Newton School/Number of strings.cpp
@@ -0,0 +1,18 @@
+https://my.newtonschool.co/playground/code/iovx7v04qjal
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    int ans = 1;
+    while(n--)
+        ans *= 3;
+    cout << ans;
+    return 0;
+}
