947311ae07e86a4864ba973c2d674bfcf239075e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-17 11:46:27 +0530

    Create What are test cases.py

---
+++ Codechef/Practice/What are test cases.py
@@ -0,0 +1,12 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP02_V2/problems/GS09
+
+
+
+
+# accept the count of test cases given in the the 1st line
+t = int(input())
+
+# run a loop to accept 't' inputs
+for i in range(t):     
+    N = int(input())      
+    print(N)
