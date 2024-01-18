cb7df734005a975e7f52fca10ef344d336848dea
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-18 11:23:54 +0530

    Create Test cases with multiple lines of input.cpp

---
+++ Codechef/Practice/Test cases with multiple lines of input.py
@@ -0,0 +1,16 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP02_V2/problems/GS10
+
+
+
+
+t = int(input())       
+
+for i in range(t):     
+    # accept 2 integers on the 1st line using map
+    A,B = map(int,input().split())
+    
+    # accept 3 integers on the 2nd line using map
+    C,D,E = map(int,input().split())
+    
+    # output the 5 integers on a single line for each test case
+    print(A, B, C, D, E)
