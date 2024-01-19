016e391655ac7886d0d90dada06ab91a2d09d7c6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-19 19:37:22 +0530

    Create Test cases with multiple types of input.cpp

---
+++ Codechef/Practice/Test cases with multiple types of input.cpp
@@ -0,0 +1,16 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP02_V2/problems/GS11
+
+
+
+
+t = int(input())
+
+for i in range(t):
+    # accept 2 integers on the 1st line of each test case
+    A, B = map(int,input().split());
+    
+    # accept 1 string on the 2nd line of each test case
+    C = input()
+    
+    # output the 2 integers and 1 string on a single line
+    print(A,B,C)
