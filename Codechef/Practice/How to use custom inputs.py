4d62665e4987b8f76193325e3139a1bc13d32a56
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-20 22:00:30 +0530

    Create How to use custom inputs.py

---
+++ Codechef/Practice/How to use custom inputs.py
@@ -0,0 +1,12 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP03_V2/problems/GS13B
+
+
+
+
+# Change the 'custom inputs' below and click 'run'
+# Click on 'Submit' once you have tried out to proceed to the next problem
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(n+1)
