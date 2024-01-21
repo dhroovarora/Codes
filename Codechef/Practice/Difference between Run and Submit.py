e79f7ddac90d81030377593c17b82ef771e5f31d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-21 17:40:21 +0530

    Create Difference between Run and Submit.py

---
+++ Codechef/Practice/Difference between Run and Submit.py
@@ -0,0 +1,12 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP03_V2/problems/GS13C
+
+
+
+
+# Change the 'custom inputs' below and click 'run'
+# Once you have run the code - click on 'submit' to find the result with the private test files
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(n+1)
