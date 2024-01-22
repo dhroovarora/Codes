890966c4c562d623d5c0e678bd9fdcab08f0a540
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-22 23:05:39 +0530

    Create Number mirror - Negative integer.py

---
+++ Codechef/Practice/Number mirror - Negative integer.py
@@ -0,0 +1,13 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP04_V2/problems/GS13
+
+
+
+
+# accept the count of test cases given in the the 1st line
+t = int(input())
+
+# run a for loop for each test case
+# then for each test case input an integer and output it's negative
+for i in range(t):
+    a = int(input())
+    print(a*(-1))
