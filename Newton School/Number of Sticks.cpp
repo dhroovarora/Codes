b395e96554c46d98277db3a4c3d64e987236d74d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-22 23:49:56 +0530

    Create Number of Sticks.cpp

---
+++ Newton School/Number of Sticks.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/2yg5esruv9me
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    cout << (n-1)/2 + 1;
+    return 0;
+}
