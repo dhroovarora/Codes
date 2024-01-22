f3bcccfcc1abca489147bd4f338aced643e3fb82
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-23 00:45:45 +0530

    Create String mirror - Double strings.py

---
+++ Codechef/Practice/String mirror - Double strings.py
@@ -0,0 +1,10 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP04_V2/problems/GS14
+
+
+
+
+t = int(input())
+for i in range(t):
+    # take input and output the join using +
+    s = input()
+    print(s+s)
