778e8791fb806cb1c4e2f6c4a722e53edee9ccf4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-25 20:39:17 +0530

    Create Debug this code  Why is this code incorrect.py

---
+++ Codechef/Practice/Debug this code  Why is this code incorrect.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP04_V2/problems/GS16
+
+
+
+
+# Debug the code below to solve the problem
+
+t = int(input())
+for i in range(t):
+    N = int(input())
+    print(2*N)
