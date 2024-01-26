5a811c913ebf94fc1daff53cbbe300860b665078
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-26 16:05:56 +0530

    Create Find XY.cpp

---
+++ Newton School/Find XY.cpp
@@ -0,0 +1,16 @@
+https://my.newtonschool.co/playground/code/wykddnqfxpyp
+
+
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int l,r;
+    cin >> l >> r;
+    (2*l <= r) ? cout << "YES" : cout << "NO";
+    return 0;
+}
