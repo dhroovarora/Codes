fbf0cbb2bd1dbc3ddd8e190c42f07cb5b2ee6ff5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-26 13:50:36 +0530

    Create Basic math - Addition and multiplication.py

---
+++ Codechef/Practice/Basic math - Addition and multiplication.py
@@ -0,0 +1,17 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP05_V2/problems/BM01
+
+
+
+
+# Update the '_' in the code below
+
+t = int(input())
+for i in range(t):
+    #Accept 2 integers inputs
+    A, B = map(int,input().split())     
+    #Sum of inputs
+    S = A + B               
+    #Product of inputs
+    P = A * B               
+    #Print the desired output for each test case
+    print(S,P)
