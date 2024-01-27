b710ea449c32893e39b9d4365f7d8f107e136235
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-27 14:22:09 +0530

    Create Practice problem - Tuition fees.py

---
+++ Codechef/Practice/Practice problem - Tuition fees.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP05_V2/problems/BM05
+
+
+
+
+# Update the code below to solve the problem
+
+t = int(input())
+for i in range(t): 
+    X, Y = map(int, input().split())
+    print(X*Y)
