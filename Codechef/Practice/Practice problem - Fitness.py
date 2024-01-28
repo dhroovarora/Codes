a17c00abb8397407a1c624c7aa1d22975e3d40a2
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-28 09:54:37 +0530

    Create Practice problem - Fitness.py

---
+++ Codechef/Practice/Practice problem - Fitness.py
@@ -0,0 +1,11 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP05_V2/problems/BM06
+
+
+
+
+# Update the code below to solve the problem
+
+t = int(input())
+for i in range(t):
+    X = int(input())
+    print(X*10)
