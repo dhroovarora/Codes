58151f58b8654a725e7395720fded563eed4c708
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-01-29 22:50:25 +0530

    Create Is it a 'YES'.cpp

---
+++ Newton School/Is it a 'YES'.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/sd5skn0qfn0r
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    string s;
+    cin >> s;
+    (tolower(s[0]) == 'y' && tolower(s[1]) == 'e' && tolower(s[2]) == 's') ? cout << "Yes" : cout << "No";
+    return 0;
+}
