1f303c463108d6ce362abce34f20e5f21b098b22
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-02 19:35:28 +0530

    Create Even Odd Game.cpp

---
+++ Newton School/Even Odd Game.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/trvtkyoh2fs2
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    (n%2) ? cout << "Tia" : cout << "Raj";
+    return 0;
+}
