0237931ea4533be029e5e204b60aa12ef00214e9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-03 13:11:58 +0530

    Create Practice problem - Tax in Chefland.py

---
+++ Codechef/Practice/Practice problem - Tax in Chefland.py
@@ -0,0 +1,14 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP09_V2/problems/CS03A
+
+
+
+
+# Update the code below to solve the problem
+
+t = int(input())           
+for i in range(t):
+    X = int(input())
+    if X > 100:
+        print(X-10)
+    else:
+        print(X)
