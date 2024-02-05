c3b39093a60a59def080b73f5f1db981d63a07f0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-05 22:29:16 +0530

    Create Problem (Make Avg) - Solve sub-components.py

---
+++ Codechef/Practice/Problem (Make Avg) - Solve sub-components.py
@@ -0,0 +1,16 @@
+https://www.codechef.com/learn/course/python-beginner-v2-p1/BP00BP12_V2/problems/LB01D?tab=statement
+
+
+
+
+# How to find the average of 2 numbers?
+
+A = 15
+C = 19
+B = (A + C)//2
+print(B)
+
+A = 16
+C = 18
+B = (A + C)//2
+print(B)
