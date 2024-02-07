282ee8d60e83a57f7b3c0f0298fd6f6575bee0d9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-07 11:54:58 +0530

    Create Fitness.py

---
+++ Codechef/Practice/Fitness.py
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/basic-math-python/PPYBM01/problems/FIT
+
+
+
+
+t = int(input())
+
+while t > 0:
+    x = int(input())
+    t -= 1
+    print(x*10)
+
+# Your code goes here
