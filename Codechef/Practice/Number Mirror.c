300200b779d81d6c441a3c60050563b0f4dd9bfe
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-14 12:45:47 +0530

    Create Number Mirror.c

---
+++ Codechef/Practice/Number Mirror.c
@@ -0,0 +1,14 @@
+https://www.codechef.com/practice/course/c/PCL01/problems/START01
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	printf("%d",n);
+	return 0;
+}
