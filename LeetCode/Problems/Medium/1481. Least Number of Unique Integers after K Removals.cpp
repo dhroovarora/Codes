2749912dbaf6e84ebe5ea60b5e11ad947ed32e1e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-16 23:27:20 +0530

    Create 1481. Least Number of Unique Integers after K Removals.cpp

---
+++ LeetCode/Problems/Medium/1481. Least Number of Unique Integers after K Removals.cpp
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16
+
+
+
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        for (int& x : arr) {
+            mp[x]++;
+        }
+        
+        vector<int> freq;
+        for (auto& pair : mp) {
+            freq.push_back(pair.second);
+        }
+        
+        sort(begin(freq), end(freq));
+        
+        int count = 0;
+        for (int& f : freq) {
+            if (k >= f) {
+                k -= f;
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return freq.size() - count;
+    }
+};
