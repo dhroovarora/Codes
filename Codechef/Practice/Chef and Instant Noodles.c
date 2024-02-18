6dfce6fc71b6c3c1eefa516e665e094eec585e0a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-18 11:31:32 +0530

    Create Chef and Instant Noodles.c

---
+++ Codechef/Practice/Chef and Instant Noodles.c
@@ -0,0 +1,14 @@
+https://www.codechef.com/practice/course/c/PCL01/problems/INSTNOODLE
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d",a*b);
+	return 0;
+}
