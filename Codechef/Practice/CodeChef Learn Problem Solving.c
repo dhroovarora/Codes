415500710ae89995b8670d9707ce096cf3240039
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-19 11:48:13 +0530

    Create CodeChef Learn Problem Solving.c

---
+++ Codechef/Practice/CodeChef Learn Problem Solving.c
@@ -0,0 +1,14 @@
+https://www.codechef.com/practice/course/c/PCL01/problems/CCLEARN?tab=statement
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	printf("%d",2*n);
+	return 0;
+}
