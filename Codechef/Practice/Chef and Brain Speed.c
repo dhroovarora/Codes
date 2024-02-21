c106800d6c1d7e8f01c0caf35f1b52277f16f13f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-21 11:52:26 +0530

    Create Chef and Brain Speed.c

---
+++ Codechef/Practice/Chef and Brain Speed.c
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/course/c/PCL03/problems/CBSPEED?tab=statement
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+    if(x < y){
+        printf("%s\n","YES");
+    }
+    else{
+        printf("%s\n","NO");
+    }
+    return 0;
+}
