b7bc42ef02de50842e124022ffa1dec80f519dcc
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-02-27 12:50:07 +0530

    Create Add Two Numbers.c

---
+++ Codechef/Practice/Add Two Numbers.c
@@ -0,0 +1,21 @@
+https://www.codechef.com/practice/course/c/PCL05/problems/FLOW001
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int a, b;
+
+        scanf("%d %d", &a, &b);
+
+        // Your code for each test case goes here 
+        printf("%d\n",a+b);
+    }
+}
