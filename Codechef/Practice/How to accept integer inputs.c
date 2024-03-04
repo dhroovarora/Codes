8f5e0501d184ea59228b1f29707c8762fb86ed7c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-04 22:32:47 +0530

    Create How to accept integer inputs.c

---
+++ Codechef/Practice/How to accept integer inputs.c
@@ -0,0 +1,15 @@
+https://www.codechef.com/learn/course/c-beginner-v2-p1/BC01BC01_V2/problems/GSCP003
+
+
+
+
+//update the _ in the code
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n );
+    printf("%d", n );
+  
+    return 0;
+}
