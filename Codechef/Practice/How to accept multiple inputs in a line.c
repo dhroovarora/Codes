4e5f77f74bd77610909f94ac6a237d0e8de98536
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-05 01:24:39 +0530

    Create How to accept multiple inputs in a line.c

---
+++ Codechef/Practice/How to accept multiple inputs in a line.c
@@ -0,0 +1,14 @@
+https://www.codechef.com/learn/course/c-beginner-v2-p1/BC01BC01_V2/problems/GSCP06
+
+
+
+
+//Update the blanks below to solve the problem
+#include <stdio.h>
+
+int main() {
+    int A, B, C;
+    scanf("%d%d%d", &A , &B , &C );
+    printf("%d %d %d", A, B, C);
+    return 0;
+}
