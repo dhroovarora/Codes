978583a9ae76a9f18ef8e12301255a625f166c6b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-06 21:12:24 +0530

    Create How to accept string inputs.c

---
+++ Codechef/Practice/How to accept string inputs.c
@@ -0,0 +1,18 @@
+https://www.codechef.com/learn/course/c-beginner-v2-p1/BC01BC01_V2/problems/GSCP08?tab=statement
+
+
+
+
+//fill the _ in the IDE to continue
+#include <stdio.h>
+
+int main() {
+    char A[10], B[10];
+    char C[10], D[10], E[10];
+    char F[10], G[10],H[10],I[10];
+    scanf("%s%s", A , B );
+    scanf("%s%s%s", C, D, E );
+    scanf("%s%s%s%s", F , G, H, I );
+    printf("%s %s %s %s %s %s %s %s %s", A , B , C , D , E , F , G , H , I );  
+    return 0;
+}
