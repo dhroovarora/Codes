002fd4fac7b0de4708744a79b2039cdc28478d63
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-08 22:54:45 +0530

    Create Input mirror.c

---
+++ Codechef/Practice/Input mirror.c
@@ -0,0 +1,18 @@
+https://www.codechef.com/learn/course/c-beginner-v2-p1/BC01BC01_V2/problems/GSCP05
+
+
+
+
+//Update the _ in the IDE 
+#include <stdio.h>
+
+int main() {
+    int N;
+    char  S[30];
+    scanf("%d", &N);
+    scanf("%s", S);
+    printf("%d ", N );  
+    printf("%s ", S );  
+
+    return 0;
+}
