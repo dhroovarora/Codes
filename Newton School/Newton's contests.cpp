7c4ce2cc1cfaf63c1cf91049e4d97e6065d39b53
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-09 23:19:46 +0530

    Create Newton's contests.cpp

---
+++ Newton School/Newton's contests.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/zebmit5y8aez
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n,m,k;
+    cin >> n >> m >> k;
+    (n <= m && n <= k) ? cout << "YES" : cout << "NO";
+    return 0;
+}
