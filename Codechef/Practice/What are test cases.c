fc272d458ed6eba9a68aeed1e22be81e9760ec3c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-09 23:20:16 +0530

    Create What are test cases.c

---
+++ Codechef/Practice/What are test cases.c
@@ -0,0 +1,23 @@
+https://www.codechef.com/learn/course/c-beginner-v2-p1/BC01BC02_V2/problems/GSCP09A
+
+
+
+
+//Update the _ in the code
+
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D, E;
+    scanf("%d \n", &A );
+    scanf("%d \n", &B );
+    scanf("%d \n", &C );
+    scanf("%d \n", &D );
+    scanf("%d \n", &E );
+    printf("%d \n", A );  
+    printf("%d \n", B );  
+    printf("%d \n", C );  
+    printf("%d \n", D );  
+    printf("%d \n", E );  
+    
+}
