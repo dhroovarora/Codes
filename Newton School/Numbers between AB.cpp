7c64d90206c6eeee5a7708eadefb57e500c92c0f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-14 22:53:32 +0530

    Create Numbers between AB.cpp

---
+++ Newton School/Numbers between AB.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/rgtpgxmywu46
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a,b;
+    cin >> a >> b;
+    cout << abs(a-b) + 1;
+    return 0;
+}
