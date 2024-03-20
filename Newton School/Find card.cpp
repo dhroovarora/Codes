3047884e4eb1fdefeddac022dadedcd03b9f1eaa
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-20 23:59:26 +0530

    Create Find card.cpp

---
+++ Newton School/Find card.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/wshzk7arutg0
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n,i;
+    cin >> n >> i;
+    cout << n - i + 1;
+    return 0;
+}
