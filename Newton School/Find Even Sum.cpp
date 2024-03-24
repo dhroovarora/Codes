2f078108fc40e18e73ef20104a643c69160f0e3e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-24 22:34:36 +0530

    Create Find Even Sum.cpp

---
+++ Newton School/Find Even Sum.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/b8x6q6nkgywu
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n,m;
+    cin >> n >> m;
+    cout << ((n-1)*n)/2 + ((m-1)*m)/2;
+    return 0;
+}
