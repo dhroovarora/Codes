bf61d8b560606c29d7db7c2bd1e1d41102f0165b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-24 22:19:16 +0530

    Create Insert an Element at the Bottom of a Stack.cpp

---
+++ GeeksForGeeks/Easy/Insert an Element at the Bottom of a Stack.cpp
@@ -0,0 +1,23 @@
+https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1
+
+
+
+
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x){
+        stack<int> s;
+        while(!st.empty()){
+            s.push(st.top());
+            st.pop();
+        }
+        st.push(x);
+        
+        while(!s.empty()){
+            st.push(s.top());
+            s.pop();
+        }
+        
+        return st;
+    }
+};
