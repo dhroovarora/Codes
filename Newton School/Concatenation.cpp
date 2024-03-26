5a898dcbc22fb848c07d74badf88b36c19f0b991
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-26 21:41:49 +0530

    Create Concatenation.cpp

---
+++ Newton School/Concatenation.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/cb5gbkigsgig
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    string p,q;
+    cin >> p >> q;
+    cout << q << '-' << p;
+    return 0;
+}
