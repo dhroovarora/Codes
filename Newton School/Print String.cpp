6aa886375f729e63bf947e517bf57ae51af436f5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-27 23:32:59 +0530

    Create Print String.cpp

---
+++ Newton School/Print String.cpp
@@ -0,0 +1,17 @@
+https://my.newtonschool.co/playground/code/jor034y4i1lf
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    for(int i=0;i<26;++i){
+        int a;
+        cin >> a;
+        cout << char('a' + a-1);
+    }
+    return 0;
+}
