e9763be997649b08e2a58c6503195268ea2ff1e4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-03-29 23:04:34 +0530

    Create Euler Circuit in an Undirected Graph.cpp

---
+++ GeeksForGeeks/Medium/Euler Circuit in an Undirected Graph.cpp
@@ -0,0 +1,18 @@
+https://www.geeksforgeeks.org/problems/euler-circuit-in-a-directed-graph/1
+
+
+
+
+class Solution {
+public:
+bool isEularCircuitExist(int v, vector<int>adj[]){
+   
+   
+   for(int i=0;i<v;i++){
+       if(adj[i].size()%2==1)
+       return 0;
+   }
+   return 1;
+}
+ 
+};
