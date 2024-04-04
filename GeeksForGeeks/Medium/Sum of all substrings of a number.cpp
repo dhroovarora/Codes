712b690b483e5e283c05fe844c2a5bf439529a35
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-04-04 17:41:26 +0530

    Create Sum of all substrings of a number.cpp

---
+++ GeeksForGeeks/Medium/Sum of all substrings of a number.cpp
@@ -0,0 +1,23 @@
+https://www.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1
+
+
+
+
+class Solution
+{
+    public:
+    int mod = 1e9+7;
+    long long sumSubstrings(string s)
+    {
+        int n= s.size();
+        vector<long long> dp(n,0);
+        dp[0] = s[0]-'0';
+        long long ans = dp[0];
+        for(int i=1;i<n;i++)
+        {
+            dp[i] = ((dp[i-1]*10)%mod + ((s[i]-'0')*(i+1))%mod)%mod;
+            ans = (ans + dp[i])%mod;
+        }
+        return ans;
+    }
+};
