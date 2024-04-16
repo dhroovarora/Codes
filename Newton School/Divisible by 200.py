98af9e2c6041cc20630c7c1701ee96eebf024c33
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-04-16 23:51:05 +0530

    Create Divisible by 200.py

---
+++ Newton School/Divisible by 200.py
@@ -0,0 +1,12 @@
+https://my.newtonschool.co/playground/code/mrjtplfsfz2b
+
+
+
+
+n,k=map(int,input().split())
+for i in range(k) :
+    if n%200==0 :
+        n=n//200
+    else :
+        n=int(str(n)+str(200))
+print(n)
