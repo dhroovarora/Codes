c4027d65a3d55d8d92ae889afa772e23e4df1f67
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-04-21 16:33:05 +0530

    Create Three way partitioning.cpp

---
+++ GeeksForGeeks/Easy/Three way partitioning.cpp
@@ -0,0 +1,28 @@
+https://www.geeksforgeeks.org/problems/three-way-partitioning/1
+
+
+
+
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& arr,int a, int b)
+    {
+        // code here 
+        int n=arr.size();
+        int l=0, r=n-1, i=0;
+        while(i<=r){
+            if(arr[i]<a){
+                swap(arr[l],arr[i]);
+                l++;
+            }
+            else if(arr[i]>b){
+                swap(arr[i],arr[r]);
+                r--;
+                i--;
+            }
+            i++;
+        }
+    }
+};
