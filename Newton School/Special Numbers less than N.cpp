e7d033e1806fc7c1f3ea74ae3bcb2ed3c02855cf
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-04-24 22:05:49 +0530

    Create Special Numbers less than N.cpp

---
+++ Newton School/Special Numbers less than N.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/18bt5pxwy1xu
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    cout << (n+1)/10;
+    return 0;
+}
