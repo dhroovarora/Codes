65bc62cc5a09bf48dc6962779f4e7b606c9f697f
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-04-30 21:47:37 +0530

    Create 1915. Number of Wonderful Substrings.cpp

---
+++ LeetCode/Problems/Medium/1915. Number of Wonderful Substrings.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/number-of-wonderful-substrings/description/?envType=daily-question&envId=2024-04-30
+
+
+
+
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        unordered_map<int, int> count;
+        int mask = 0;
+        count[0] = 1;
+        long long result = 0;
+        for(char c : word) {
+            mask ^= 1 << (c - 'a');
+            result += count[mask];
+            for(int i = 0; i < 10; i++) {
+                result += count[mask ^ (1 << i)];
+            }
+            count[mask]++;
+        }
+        return result;
+    }
+};
