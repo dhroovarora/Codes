92f1b9a4115f004586ecb4e24a1dffbd13c4cbe2
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-03 23:47:59 +0530

    Create Fix zero.cpp

---
+++ Newton School/Fix zero.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/v6fen3g06rpa
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    (a == 0) ? cout << 1 : (b==0) ? cout << 2 : (c==0) ? cout << 3 : (d==0) ? cout << 4 : cout << 5;
+    return 0;
+}
