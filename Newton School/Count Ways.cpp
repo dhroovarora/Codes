f4f567dbc4be48a33072df477cc0786c71df94d6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-08 08:02:09 +0530

    Create Count Ways.cpp

---
+++ Newton School/Count Ways.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/x8lxm64cquvl
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    cout << n/2 + 1;
+    return 0;
+}
