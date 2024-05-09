e95ffce72c59b907cfb857c44923dee2d80a273c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-09 14:26:36 +0530

    Create Divisor Game.cpp

---
+++ GeeksForGeeks/Easy/Divisor Game.cpp
@@ -0,0 +1,12 @@
+https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+
+
+
+class Solution {
+  public:
+    bool divisorGame(int n) {
+        // code here
+        return n % 2 == 0; 
+    }
+};
