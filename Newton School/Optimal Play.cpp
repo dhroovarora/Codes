1645c205853bd855cc875d3ac66bdfe0f035484c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-11 20:20:12 +0530

    Create Optimal Play.cpp

---
+++ Newton School/Optimal Play.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/za5wxfyn0df6
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    (n&1) ? cout << 0 : cout << 1;
+    return 0;
+}
