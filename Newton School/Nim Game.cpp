40f09b89994c1dd24c804695ffb5128e2b7627fd
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-16 11:53:52 +0530

    Create Nim Game.cpp

---
+++ Newton School/Nim Game.cpp
@@ -0,0 +1,15 @@
+https://my.newtonschool.co/playground/code/w0ifi12z395k
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n;
+    cin >> n;
+    (n%4) ? cout << "true" : cout << "false";
+    return 0;
+}
