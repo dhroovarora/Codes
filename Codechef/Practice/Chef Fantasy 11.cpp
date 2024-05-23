238200a6363947048b291b4e1d52c372c8693c9d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-05-23 18:39:52 +0530

    Create Chef Fantasy 11.cpp

---
+++ Codechef/Practice/Chef Fantasy 11.cpp
@@ -0,0 +1,18 @@
+https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FIZZBUZZ2303?tab=statement
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << n*(n-1) << endl;
+	}
+}
