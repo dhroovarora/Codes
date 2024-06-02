b8a62ee9b28dc99f1861880d54480b7ab03855e5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-02 12:16:23 +0530

    Create Songs.java

---
+++ Newton School/Songs.java
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/jcbt6sfgift0
+
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a/(3*b));
+    }
+}
