c7474ff3b35025ef47da6b6feba6510f22975618
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-10 05:51:07 +0530

    Create Nuts and Bolts Problem.java

---
+++ GeeksForGeeks/Medium/Nuts and Bolts Problem.java
@@ -0,0 +1,15 @@
+https://www.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+
+
+
+
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    // code here
+	    sort(nuts,nuts + n);
+	    sort(bolts,bolts + n);
+	}
+
+};
