d32870a771cf1db35624c0d2b5c7ed53b944520b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-17 22:47:03 +0530

    Create IPL Qualification.cpp

---
+++ Newton School/IPL Qualification.cpp
@@ -0,0 +1,15 @@
+https: // my.newtonschool.co/playground/code/i6mh1if77on7
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int x,y;
+    cin >> x >> y;
+    cout << max(0,x-y);
+    return 0;
+}
