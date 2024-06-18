dbd145bc4e1248bb9966d5b084f74b252cfd8cd5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-18 17:37:30 +0530

    Create Number of Rectangles in a Circle.java

---
+++ GeeksForGeeks/Medium/Number of Rectangles in a Circle.java
@@ -0,0 +1,15 @@
+https://www.geeksforgeeks.org/problems/rectangles-in-a-circle0457/1
+
+
+
+
+class Solution{
+    int rectanglesInCircle(int r){
+        int ans=0;
+        int r1 = 4*r*r;
+        for(int i=1;i<2*r;i++){
+            ans += Math.abs(Math.sqrt(r1 - i*i));
+        }
+        return ans;
+    }
+};
