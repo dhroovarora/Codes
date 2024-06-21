a4379a4e5310534f5d6f3844d1039c32eab30df4
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-22 01:19:49 +0530

    Create Mask Policy.cpp

---
+++ Newton School/Mask Policy.cpp
@@ -0,0 +1,15 @@
+https: // my.newtonschool.co/playground/code/m18gpuiqcpri
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int n,a;
+    cin >> n >> a;
+    cout << min(a,n-a);
+    return 0;
+}
