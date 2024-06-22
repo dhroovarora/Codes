897b22883e46130ed3a380d23bb01742724284d8
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-06-22 08:23:23 +0530

    Create 1248. Count Number of Nice Subarrays copy.java

---
+++ LeetCode/Problems/Medium/1248. Count Number of Nice Subarrays copy.java
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/count-number-of-nice-subarrays/description/?envType=daily-question&envId=2024-06-22
+
+
+
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] cnt = new int[n + 1];
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for (int v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += cnt[t - k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+}
