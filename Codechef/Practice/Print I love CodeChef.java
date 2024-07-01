d98698f0bd9a5b98fe088cb29a466356a471a9f8
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-01 22:02:33 +0530

    Create Print I love CodeChef.java

---
+++ Codechef/Practice/Print I love CodeChef.java
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS19
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("I");
+        System.out.println("love");
+        System.out.println("CodeChef");
+    }
+}
