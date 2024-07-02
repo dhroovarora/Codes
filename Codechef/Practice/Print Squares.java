ef333c488719a189714788dec99c00a314ef6090
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-02 23:05:06 +0530

    Create Print Squares.java

---
+++ Codechef/Practice/Print Squares.java
@@ -0,0 +1,15 @@
+https://www.codechef.com/practice/course/java/LPJAAS02/problems/LJAAS20
+
+
+
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+	    for(int i=1;i<=5;++i){
+	        System.out.println(i + " - " + (i*i));
+	    }
+	}
+}
