52525d2850f3195cf39e77045ca801aaf7e1241e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-03 22:55:00 +0530

    Create Dinner Preparation.cpp

---
+++ Newton School/Dinner Preparation.cpp
@@ -0,0 +1,20 @@
+https://my.newtonschool.co/playground/code/u2cgv0euierd
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        (a <= b) ? cout << "Shyam" : cout << "Ram";
+        cout << endl;
+    }
+    return 0;
+}
