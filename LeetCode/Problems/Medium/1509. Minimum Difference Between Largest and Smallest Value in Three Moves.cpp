658b26a375d4f38949570e4a1612389aa38b06b3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-03 22:50:31 +0530

    Create 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp

---
+++ LeetCode/Problems/Medium/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -0,0 +1,26 @@
+https: // leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/description/?envType=daily-question&envId=2024-07-03
+
+
+
+
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+
+        int minVal = INT_MAX;
+
+        int n = nums.size();
+
+        if(n <= 4) return 0;
+
+        minVal = min(minVal, nums[n-4] - nums[0]);
+        minVal = min(minVal, nums[n-1] - nums[3]);
+        minVal = min(minVal, nums[n-2] - nums[2]);
+        minVal = min(minVal, nums[n-3] - nums[1]);
+
+        return minVal;
+        
+    }
+};
