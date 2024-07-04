436beafdc375c361fa3f995e862031ff1b138067
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-04 20:48:26 +0530

    Create Duplicate Subtrees.cpp

---
+++ GeeksForGeeks/Medium/Duplicate Subtrees.cpp
@@ -0,0 +1,25 @@
+https://www.geeksforgeeks.org/problems/duplicate-subtrees/1
+
+
+
+
+class Solution {
+  public:
+    unordered_map<string,int> mpp;
+    vector<Node*> ans;
+    vector<Node*> printAllDups(Node* root) {
+        dfs(root);
+        return ans;
+    }
+    string dfs(Node* root) {
+        if (root == nullptr) {
+            return "#";
+        }
+        string s = to_string(root->data) + "," + dfs(root->left) + "," + dfs(root->right);
+        if (mpp[s] == 1) {
+            ans.push_back(root);
+        }
+        mpp[s]++;
+        return s;
+    }
+};
