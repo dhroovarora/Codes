7df010d92d24ae6a2395b81b5fc2823aa8f4aa34
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-05 22:20:43 +0530

    Create Vertical Width of a Binary Tree.cpp

---
+++ GeeksForGeeks/Medium/Vertical Width of a Binary Tree.cpp
@@ -0,0 +1,25 @@
+https://www.geeksforgeeks.org/problems/vertical-width-of-a-binary-tree/1
+
+
+
+
+class Solution {
+  public:
+    void solve(Node* root, unordered_map<int, int> &mp, int width){
+        if(!root) return ;
+        
+        mp[width]++;
+        
+        solve(root->left, mp, width-1);
+        solve(root->right, mp, width+1);
+    }
+    // Function to find the vertical width of a Binary Tree.
+    int verticalWidth(Node* root) {
+        // code here
+        unordered_map<int, int> mp;
+        
+        solve(root, mp, 0);
+        return mp.size();
+        
+    }
+};
