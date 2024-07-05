59f828da144ea5ea3ccec07c0894b43ce377676a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-05 22:33:40 +0530

    Create Exams.cpp

---
+++ Newton School/Exams.cpp
@@ -0,0 +1,15 @@
+https://app.studystream.live/focus/room
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int x,y,z;
+    cin >> x >> y >> z;
+    (2*z > (x*y)) ? cout << "Yes" : cout << "No";
+    return 0;
+}
