47c249996bfe3aa102aff6bb059f0e7e1722958a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-11 21:06:52 +0530

    Create Divide two variables.java

---
+++ Codechef/Practice/Divide two variables.java
@@ -0,0 +1,13 @@
+https://www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS29
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here 
+        int x=20;
+        int y=6;
+        System.out.print(x/y);
+    }
+}
