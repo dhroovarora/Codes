322f04b91a31d42baccf04b6b2fd6d5e06422426
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-13 15:19:32 +0530

    Create Convert Temperature.cpp

---
+++ Codechef/Practice/Convert Temperature.cpp
@@ -0,0 +1,13 @@
+https: // www.codechef.com/practice/course/java/LPJAAS03/problems/LJAAS30?tab=statement
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here
+        double temperature = 25.5;
+        System.out.println("Celsius - "+temperature);
+        System.out.print("Kelvin - "+(temperature+273));
+    }
+}
