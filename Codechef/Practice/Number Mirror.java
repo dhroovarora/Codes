a0f85e81b2e2c979425e98b695c3e6bcf1cc14f1
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-15 21:31:14 +0530

    Create Number Mirror.java

---
+++ Codechef/Practice/Number Mirror.java
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/course/java/PJA01/problems/START01
+
+
+
+
+import java.util.Scanner; 
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// Print the value of n
+		System.out.print(n);
+		
+	}
+}
