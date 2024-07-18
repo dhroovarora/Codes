35d1b53377f4cfcc74d9fec5adde31464cce23d0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-18 07:29:38 +0530

    Create Chef and Instant Noodles.java

---
+++ Codechef/Practice/Chef and Instant Noodles.java
@@ -0,0 +1,20 @@
+https://www.codechef.com/practice/course/java/PJA01/problems/INSTNOODLE
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a*b);
+	}
+}
