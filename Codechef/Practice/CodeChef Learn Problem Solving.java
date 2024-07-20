bb08c6a27fadfcbf19dbaca12de966d2b87ead77
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-20 12:12:05 +0530

    Create CodeChef Learn Problem Solving.java

---
+++ Codechef/Practice/CodeChef Learn Problem Solving.java
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/course/java/PJA01/problems/CCLEARN
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(n*2);
+	}
+}
