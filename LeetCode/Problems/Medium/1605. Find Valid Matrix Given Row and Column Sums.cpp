bd219bed4f2f749f1d1ae970414f629dc826d12b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-20 12:07:22 +0530

    Create 1605. Find Valid Matrix Given Row and Column Sums.cpp

---
+++ LeetCode/Problems/Medium/1605. Find Valid Matrix Given Row and Column Sums.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/description/?envType=daily-question&envId=2024-07-20
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int numRows = rowSum.size();
+        int numCols = colSum.size();
+        vector<vector<int>> result(numRows, vector<int>(numCols, 0));
+
+        int i = 0, j = 0;
+
+        while (i < numRows && j < numCols) {
+            int val = min(rowSum[i], colSum[j]);
+            result[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+
+            if (rowSum[i] == 0) {
+                i++;
+            }
+            if (colSum[j] == 0) {
+                j++;
+            }
+        }
+        return result;
+    }
+};
