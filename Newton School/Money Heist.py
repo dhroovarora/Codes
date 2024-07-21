764c13964b92915c09709426ca701b36778660a9
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-21 21:56:28 +0530

    Create Money Heist.py

---
+++ Newton School/Money Heist.py
@@ -0,0 +1,18 @@
+https://my.newtonschool.co/playground/code/wvh9nox7vqgv
+
+
+
+
+# Your code here
+D,d,p,q=map(int,input().split())
+c=0
+i=1
+m=0
+while i <= D :
+    m +=p
+    c +=1
+    if c==d :
+        p +=q 
+        c=0
+    i +=1
+print(m)
