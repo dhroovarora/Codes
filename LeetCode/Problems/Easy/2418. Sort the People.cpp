3acb4dd0cd715228796b4ea9c224fbfbc1582728
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-22 22:08:32 +0530

    Create 2418. Sort the People.cpp

---
+++ LeetCode/Problems/Easy/2418. Sort the People.cpp
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/sort-the-people/description/?envType=daily-question&envId=2024-07-22
+
+
+
+
+class Solution {
+public:
+    vector<std::string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+        unordered_map<int, string> mapping;
+
+        for (int i = 0; i < n; ++i) {
+            mapping[heights[i]] = names[i];
+        }
+
+        sort(heights.rbegin(), heights.rend());
+
+        for (int i = 0; i < n; ++i) {
+            names[i] = mapping[heights[i]];
+        }
+
+        return names;
+    }
+};
