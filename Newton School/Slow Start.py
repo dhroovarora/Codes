1ce8dfdb8c5ee57b188bf886f753033f11de8065
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-22 22:27:17 +0530

    Create Slow Start.py

---
+++ Newton School/Slow Start.py
@@ -0,0 +1,17 @@
+https://my.newtonschool.co/playground/code/yg9hhfjg4tda
+
+
+
+
+# Your code here
+for _ in range(int(input())) :
+    x,h=map(int,input().split())
+    c=0
+    s=0
+    while s < h :
+        if c < 5 :
+            s += x//2
+        else :
+            s +=x 
+        c +=1
+    print(c)
