cf59f7c34479d58c15cbac4befc8c0b320d30ea2
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-23 20:13:19 +0530

    Create Sort it.py

---
+++ Newton School/Sort it.py
@@ -0,0 +1,14 @@
+https://my.newtonschool.co/playground/code/vpbi206n8jcs
+
+
+
+
+# Your code here
+# Your code here
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    a=list(map(int,input().split()))
+    b=list(map(int,input().split()))
+    for x in b :
+        a=a[:n-x] + sorted(a[n-x:])
+    print (" ".join(str(x) for x in a))
