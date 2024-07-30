96c6fd4a5c1f3bb95bd7f54ff7993cc06e9cff94
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-30 21:38:06 +0530

    Create Ageing.cpp

---
+++ Newton School/Ageing.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/bji63fd74ipz
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        cout << 10 + (x-20) << endl;
+    }
+    return 0;
+}
