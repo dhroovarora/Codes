be4de6b5a7d5b7c0d4fc1dc920004abaaa109b95
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-07-30 21:35:13 +0530

    Create Grades of Student.cpp

---
+++ Codechef/Practice/Grades of Student.cpp
@@ -0,0 +1,56 @@
+https://www.codechef.com/practice/course/java/LPJAAS06/problems/LJAAS60
+
+
+
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num > 90){
+		    System.out.println("A");
+		}
+		else if(num > 70){
+		    System.out.println("B");
+		}
+		else if(num >=40){
+		    System.out.println("C");
+		}
+		else{
+		    System.out.println("F");
+		}
+		
+		num = sc.nextInt();
+		if(num > 90){
+		    System.out.println("A");
+		}
+		else if(num > 70){
+		    System.out.println("B");
+		}
+		else if(num >=40){
+		    System.out.println("C");
+		}
+		else{
+		    System.out.println("F");
+		}
+		
+		num = sc.nextInt();
+		if(num > 90){
+		    System.out.println("A");
+		}
+		else if(num > 70){
+		    System.out.println("B");
+		}
+		else if(num >=40){
+		    System.out.println("C");
+		}
+		else{
+		    System.out.println("F");
+		}
+	}
+}
