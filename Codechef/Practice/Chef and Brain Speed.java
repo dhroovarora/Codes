4a57cb75cf03d43f692796d9249ec535b8083c11
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-01 20:33:58 +0530

    Create Chef and Brain Speed.java

---
+++ Codechef/Practice/Chef and Brain Speed.java
@@ -0,0 +1,27 @@
+https://www.codechef.com/practice/course/java/PJA03/problems/CBSPEED?tab=statement
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		if(x < y){
+		    System.out.print("YES");
+		}
+		else{
+		    System.out.print("NO");
+		}
+	}
+}
