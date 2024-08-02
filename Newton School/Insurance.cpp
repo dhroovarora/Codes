48ff27d8392315e5a676b7ef5513dffa7c077b70
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-02 21:34:23 +0530

    Create Insurance.cpp

---
+++ Newton School/Insurance.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/jhqj443pe2kd
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << min(x,y) << endl;
+    }
+    return 0;
+}
