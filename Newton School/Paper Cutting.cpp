6edadf315dc910e557b23a8b45134d7ed041642e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-03 07:45:58 +0530

    Create Paper Cutting.cpp

---
+++ Newton School/Paper Cutting.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/fgtawlrecfki
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        cout << (n/k)*(n/k) << endl;
+    }
+    return 0;
+}
