ba0ce30135cb4bca071555b78bf9ea8a6484ef06
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-05 20:55:16 +0530

    Create Oneful Pairs.java

---
+++ Codechef/Practice/Oneful Pairs.java
@@ -0,0 +1,28 @@
+https://www.codechef.com/practice/course/java/PJA03/problems/ONEFULPAIRS
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		// write your code here
+		if(a+b+a*b == 111){
+		    System.out.print("Yes");
+		}
+		else{
+		    System.out.print("No");
+		}
+		
+	}
+}
