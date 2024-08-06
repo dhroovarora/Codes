3539d418a17f3e5df868cec5bff89a4c0927056e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-06 20:33:01 +0530

    Create 3016. Minimum Number of Pushes to Type Word II.cpp

---
+++ LeetCode/Problems/Medium/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/?envType=daily-question&envId=2024-08-06
+
+
+
+
+class Solution {
+public:
+    static int minimumPushes(string& word) {
+        int freq[26]={0};
+        for(char c: word) 
+            freq[c-'a']++;
+        sort(freq, freq+26, greater<int>());
+        int sz=lower_bound(freq, freq+26, 0, greater<int>())-freq, i=0; 
+        return accumulate(freq, freq+sz, 0,  [&i](int sum, int f){
+            return sum+=(f*(i++/8+1));
+        });
+    }
+};
