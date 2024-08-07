84be15e34c2095fc41ece0a585ab4efd361726be
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-07 21:46:07 +0530

    Create Counting Problem.py

---
+++ Newton School/Counting Problem.py
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/hl6x7fvxlr3f
+
+
+
+
+# Your code here
+for _ in range(int(input())) :
+    n=int(input())
+    l=list(map(int,input().split()))
+    o,e=0,0
+    for x in l :
+        if x%2==0 :
+            e +=1
+        else :
+            o +=1
+    if o%2==0 :
+        print("Yes")
+    else :
+        print("No")
