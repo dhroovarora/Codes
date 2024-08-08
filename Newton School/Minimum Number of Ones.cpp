9bac6e48bef54b3d4cdc390f3cda394459a4dc10
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-08 19:20:34 +0530

    Create Minimum Number of Ones.cpp

---
+++ Newton School/Minimum Number of Ones.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/ybjtybp60exw
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n/2 << endl;
+    }
+    return 0;
+}
