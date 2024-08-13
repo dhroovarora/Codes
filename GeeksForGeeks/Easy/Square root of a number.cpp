57f6517381cb3f335931fab4e2a4bc93d0fe6deb
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-13 21:38:43 +0530

    Create Square root of a number.cpp

---
+++ GeeksForGeeks/Easy/Square root of a number.cpp
@@ -0,0 +1,12 @@
+https://www.geeksforgeeks.org/problems/square-root/1
+
+
+
+
+class Solution {
+  public:
+    long long int floorSqrt(long long int n) {
+        // Your code goes here
+        return sqrt(n);
+    }
+};
