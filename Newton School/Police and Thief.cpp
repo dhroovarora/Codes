406a50604fa7adcbdad9209e0053564c9f78866a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-16 21:40:23 +0530

    Create Police and Thief.cpp

---
+++ Newton School/Police and Thief.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/3e3n9449p3vs
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << abs(x-y) <<;
+    }
+    return 0;
+}
