afc1c90e44df8ae60e1e3cfdc9f1708b62b18683
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-18 22:52:59 +0530

    Create Split an array into two equal Sum subarrays.cpp

---
+++ GeeksForGeeks/Easy/Split an array into two equal Sum subarrays.cpp
@@ -0,0 +1,23 @@
+https://www.geeksforgeeks.org/problems/split-an-array-into-two-equal-sum-subarrays/1
+
+
+
+
+class Solution {
+  public:
+     bool canSplit(vector<int>& arr) {
+        float sum=0;
+        for(int i=0; i<arr.size(); i++){
+            sum +=arr[i];
+        }
+        if(sum == 0)
+            return 1;
+        float temp=0;
+        for(int i=0; i<arr.size(); i++){
+            temp += arr[i];
+            if(temp == sum/2)
+               return true;
+        }
+        return false;
+    }
+};
