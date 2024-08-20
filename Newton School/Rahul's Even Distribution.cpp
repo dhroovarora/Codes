c02a42fae795c0ddadd1209e1a7ffe8a55b7de9d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-20 22:14:59 +0530

    Create Rahul's Even Distribution.cpp

---
+++ Newton School/Rahul's Even Distribution.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/sy53ne4bc5n8
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        cout << __gcd(n,m) << endl;
+    }
+    return 0;
+}
