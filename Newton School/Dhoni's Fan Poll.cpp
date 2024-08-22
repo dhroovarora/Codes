9e2ea1977cc3ad309cea08d27d2411ce641d8df0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-22 23:17:22 +0530

    Create Dhoni's Fan Poll.cpp

---
+++ Newton School/Dhoni's Fan Poll.cpp
@@ -0,0 +1,20 @@
+https://my.newtonschool.co/playground/code/lugbf6m4xguc
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a>b && a>c){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+    return 0;
+}
