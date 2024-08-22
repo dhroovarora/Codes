d4b4a98dc99ebe32edf0f050ac9fb547727c7865
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-22 23:14:42 +0530

    Create Water Consumption.java

---
+++ Codechef/Practice/Water Consumption.java
@@ -0,0 +1,30 @@
+https://www.codechef.com/practice/course/java/PJA05/problems/WATERCONS
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		// write your code here
+    		if(x > 1999){
+    		    System.out.println("YES");
+    		}
+    		else{
+    		    System.out.println("NO");
+    		}
+		}
+		
+	}
+}
