07eb1ce309022f204142cd1a94b816d8e4e71b1a
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-22 23:14:17 +0530

    Create 476. Number Complement.cpp

---
+++ LeetCode/Problems/Easy/476. Number Complement.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/number-complement/description/?envType=daily-question&envId=2024-08-22
+
+
+
+
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+        
+        unsigned int mask = ~0;
+        
+        while (num & mask) {
+            mask <<= 1;
+        }
+        
+        return ~mask & ~num;
+    }
+};
