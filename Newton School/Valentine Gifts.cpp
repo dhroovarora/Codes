73a9b985734deee1c87c9e00d0bf3c9d88d0b174
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-25 22:52:54 +0530

    Create Valentine Gifts.cpp

---
+++ Newton School/Valentine Gifts.cpp
@@ -0,0 +1,20 @@
+https://my.newtonschool.co/playground/code/fpnfdbkxyzfe
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        (x>126) ? cout << "Yes" : cout << "No";
+        cout << endl;
+    }
+    return 0;
+}
