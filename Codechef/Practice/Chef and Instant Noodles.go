160c4d4bee2b11c84c8cbb391f5f82c9c67e7a33
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-27 19:29:08 +0530

    Create Chef and Instant Noodles.go

---
+++ Codechef/Practice/Chef and Instant Noodles.go
@@ -0,0 +1,16 @@
+https://www.codechef.com/practice/course/go/PGOL01/problems/INSTNOODLE?tab=statement
+
+
+
+
+package main
+import "fmt"
+
+func main(){
+	// your code goes here
+	var a,b int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	c:= a * b
+	fmt.Println(c)
+}
