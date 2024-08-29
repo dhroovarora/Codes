049bca47a1bd9c752ff1c90beff853910d212139
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-29 17:23:49 +0530

    Create X Jumps.cpp

---
+++ Newton School/X Jumps.cpp
@@ -0,0 +1,19 @@
+https://my.newtonschool.co/playground/code/qjm3cova8m7x
+
+
+
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	// Your code here
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x%y + x/y << endl;
+    }
+    return 0;
+}
