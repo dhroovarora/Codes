a73be77c222e995e2c2d025ce81f3df6df981e85
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-08-29 17:20:47 +0530

    Create CodeChef Learn Problem Solving.go

---
+++ Codechef/Practice/CodeChef Learn Problem Solving.go
@@ -0,0 +1,15 @@
+https://www.codechef.com/practice/course/go/PGOL01/problems/CCLEARN
+
+
+
+
+package main
+import "fmt"
+
+func main(){
+	// your code goes here
+	var a int
+	fmt.Scan(&a)
+	b:= a*2
+	fmt.Println(b)
+}
