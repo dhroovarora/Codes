5a9d4e395eb764610467936368fb89459f7d4a37
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-01 23:05:05 +0530

    Create 2022. Convert 1D Array Into 2D Array.cpp

---
+++ LeetCode/Problems/Easy/2022. Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/convert-1d-array-into-2d-array/description/?envType=daily-question&envId=2024-09-01
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> result(m);
+        int i;
+        switch (m * n == original.size() ? 1 : 0) {
+            case 1:
+                i = 0;
+                while (i < m) {
+                    result[i] = vector<int>(original.begin() + i * n, original.begin() + (i * n + n));
+                    i++;
+                }
+                break;
+            default:
+                return {};
+        }
+        return result;
+    }
+};
+
+static const auto kds = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
