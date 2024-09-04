09ab8137150c987d70eba21d51240f63e8119d09
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-04 19:36:05 +0530

    Create Chef and Brain Speed.go

---
+++ Codechef/Practice/Chef and Brain Speed.go
@@ -0,0 +1,19 @@
+https://www.codechef.com/practice/course/go/PGOL03/problems/CBSPEED?tab=statement
+
+
+
+
+package main
+import "fmt"
+
+func main(){
+    var x,y int
+    fmt.Scan(&x)
+    fmt.Scan(&y)
+    if x>=y{
+        fmt.Println("NO")
+    } else{
+        fmt.Println("YES")
+    }
+    
+}
