312837ed6fb8ac27087e50e850d5017616c3b846
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-12 19:38:35 +0530

    Create Middle of a Linked List.cpp

---
+++ GeeksForGeeks/Easy/Middle of a Linked List.cpp
@@ -0,0 +1,42 @@
+https://www.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+
+
+
+
+/* Link list Node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        Node *slow = head,*fast= head;
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        int dhroov = slow->data;
+        return dhroov;
+    }
+};
