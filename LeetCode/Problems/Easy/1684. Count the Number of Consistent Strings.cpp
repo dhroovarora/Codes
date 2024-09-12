41d9fda9bec3a28bb5f62adc7417c8ac2fcf29e0
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-12 19:37:25 +0530

    Create 1684. Count the Number of Consistent Strings.cpp

---
+++ LeetCode/Problems/Easy/1684. Count the Number of Consistent Strings.cpp
@@ -0,0 +1,34 @@
+https://leetcode.com/problems/count-the-number-of-consistent-strings/description/?envType=daily-question&envId=2024-09-12
+
+
+
+
+class Solution {
+public:
+    int countConsistentStrings(string& allowed, vector<string>& words) {
+        bitset<26> ASet=0;
+        for(char c: allowed)
+            ASet[c-'a']=1;
+        int cnt=0;
+        for(string& w: words){
+            bool consistent=1;
+            for(char c: w){
+                if (ASet[c-'a']==0){
+                    consistent=0;
+                    break;
+                }
+            }
+            cnt+=consistent;
+        }
+        return cnt;
+    }
+};
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
