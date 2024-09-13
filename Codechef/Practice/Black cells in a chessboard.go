4a3559896b8e70bdcf854b8f0578bfc755e17561
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-13 19:45:37 +0530

    Create Black cells in a chessboard.go

---
+++ Codechef/Practice/Black cells in a chessboard.go
@@ -0,0 +1,15 @@
+https://www.codechef.com/practice/course/go/PGOL04/problems/BLACKCEL?tab=statement
+
+
+
+
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Scanf("%d", &n)
+    fmt.Println((n*n)/2)
+    // Your code goes here 
+}
