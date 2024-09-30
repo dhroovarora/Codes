0c4f783281446edf2be60c066f67c7c4d1dfc577
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-09-30 09:13:37 +0530

    Create 1381. Design a Stack With Increment Operation.cpp

---
+++ LeetCode/Problems/Medium/1381. Design a Stack With Increment Operation.cpp
@@ -0,0 +1,35 @@
+https://leetcode.com/problems/design-a-stack-with-increment-operation/description/?envType=daily-question&envId=2024-09-30
+
+
+
+
+class CustomStack {
+public:
+    int s[1000];
+    int top, maxSz, sz;
+    CustomStack(int maxSize): top(-1), maxSz(maxSize){}
+    
+    void push(int x) {
+        if (top==maxSz-1) return;
+        s[++top]=x;
+    }
+    
+    int pop() {
+        if (top==-1) return -1;
+        return s[top--];
+    }
+    
+    void increment(int k, int val) {
+        int k0=min(k, top+1);
+        for(int i=0; i<k0; i++)
+            s[i]+=val;
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
