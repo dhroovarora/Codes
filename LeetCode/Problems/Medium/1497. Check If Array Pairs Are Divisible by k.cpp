5fca5e9f5e9394614f00d963ca70a279d7886137
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-01 20:33:52 +0530

    Create 1497. Check If Array Pairs Are Divisible by k.cpp

---
+++ LeetCode/Problems/Medium/1497. Check If Array Pairs Are Divisible by k.cpp
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/description/?envType=daily-question&envId=2024-10-01
+
+
+
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> freq(k, 0);
+        for (int num : arr) {
+            int rem = num % k;
+            if (rem < 0) {
+                rem += k;
+            }
+            freq[rem]++;
+        }
+        
+        if (freq[0] % 2 != 0) {
+            return false;
+        }
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
