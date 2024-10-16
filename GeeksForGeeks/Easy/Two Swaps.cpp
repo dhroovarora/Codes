3c5fa128564e40ac2961fda16b2cd5b4715b0ac6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-16 15:29:42 +0530

    Create Two Swaps.cpp

---
+++ GeeksForGeeks/Easy/Two Swaps.cpp
@@ -0,0 +1,17 @@
+https://www.geeksforgeeks.org/problems/two-swaps--155623/1
+
+
+
+
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        int ch=2;
+        for(int i=0, p=1; i<(arr.size()+1)/2; i++, p++){
+            if(p!=arr[i] and ch){
+                swap(arr[i], arr[arr[i]-1]); ch--;
+            }
+        }
+        return (ch==2) or (ch==0 and is_sorted(arr.begin(), arr.end()));
+    }
+};
