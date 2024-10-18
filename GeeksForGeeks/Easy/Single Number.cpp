4da3d67671b456a063a0b10b62c667f3772c6706
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-18 18:01:22 +0530

    Create Single Number.cpp

---
+++ GeeksForGeeks/Easy/Single Number.cpp
@@ -0,0 +1,20 @@
+https://www.geeksforgeeks.org/problems/single-number1014/1
+
+
+
+
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+        // code here
+        
+        int x=0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            x=x^arr[i];
+        }
+        
+        return x;
+        
+    }
+};
