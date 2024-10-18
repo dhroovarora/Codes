1a741dd0b532ca6eab14068e2bf4b30281a67e2b
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-18 18:00:29 +0530

    Create Print Squares.js

---
+++ Codechef/Practice/Print Squares.js
@@ -0,0 +1,11 @@
+https://www.codechef.com/practice/course/javascript/LPJAS02/problems/LPJSPR20
+
+
+
+
+// your code goes here
+console.log("1 - 1");
+console.log("2 - 4");
+console.log("3 - 9");
+console.log("4 - 16");
+console.log("5 - 25");
