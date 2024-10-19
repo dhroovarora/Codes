503fa6220f8edb5aa4e0a14dec81c60ba00befc3
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-19 19:40:14 +0530

    Create Print all the arithematic operations.js

---
+++ Codechef/Practice/Print all the arithematic operations.js
@@ -0,0 +1,10 @@
+https://www.codechef.com/practice/course/javascript/LPJAS02/problems/LPJSPR21
+
+
+
+
+// your code goes here
+console.log(8+4);
+console.log(8-4);
+console.log(8/4);
+console.log(8*4);
