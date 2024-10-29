72b2425869ac3fd5f64b6dfd217640685efbe92e
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-10-29 20:54:20 +0530

    Create Convert Temperature.js

---
+++ Codechef/Practice/Convert Temperature.js
@@ -0,0 +1,9 @@
+https://www.codechef.com/practice/course/javascript/LPJAS03/problems/LPJSPR31
+
+
+
+
+// your code goes here
+let temperature = 25.5;
+console.log("Celsius -",temperature);
+console.log("Kelvin -",temperature+273);
