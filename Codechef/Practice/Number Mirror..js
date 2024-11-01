24046b0572bb97ca91be61790dd255cf5d34c66c
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-01 12:54:43 +0530

    Create Number Mirror..js

---
+++ Codechef/Practice/Number Mirror..js
@@ -0,0 +1,18 @@
+https://www.codechef.com/practice/course/javascript/PRACTICEJS01/problems/START01
+
+
+
+
+// Complete the code 
+
+function numberMirror(n){
+   console.log(n);
+}
+
+// Input related code. Please do not change this.
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+   const n = parseInt(input.trim()); 
+   numberMirror(n);
+});
+
