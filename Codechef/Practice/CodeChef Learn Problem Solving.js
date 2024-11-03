c2862d04c30a092bc13d5d467fd61d4d51507cf6
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-03 22:21:08 +0530

    Create CodeChef Learn Problem Solving.js

---
+++ Codechef/Practice/CodeChef Learn Problem Solving.js
@@ -0,0 +1,17 @@
+https://www.codechef.com/practice/course/javascript/PRACTICEJS01/problems/CCLEARN
+
+
+
+
+// Complete the code
+
+function totalCourses(n){
+    console.log(n*2);
+}
+
+// Input related code. Please do not change this.
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(input) {
+   const n = parseInt(input.trim()); 
+   totalCourses(n);
+});
