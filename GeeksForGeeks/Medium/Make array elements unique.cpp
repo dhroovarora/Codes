13dcb20263e0bab955153bfa08b3104e1cf96c82
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-11 08:25:58 +0530

    Create Make array elements unique.cpp

---
+++ GeeksForGeeks/Medium/Make array elements unique.cpp
@@ -0,0 +1,21 @@
+https://www.geeksforgeeks.org/problems/make-array-elements-unique--170645/1
+
+
+
+
+class Solution {
+  public:
+    int minIncrements(vector<int> arr) {
+        sort(arr.begin(),arr.end());
+        int res = 0, maxLast = arr[0];
+        for ( int i = 1; i<arr.size(); i++ ){
+            if ( arr[i] == arr[i-1] ){
+                arr[i]++; res++;
+            }
+            if ( arr[i] < maxLast ){
+                res += maxLast-arr[i]+1;
+                arr[i] = maxLast+1;
+            } maxLast = max(maxLast, arr[i]);
+        } return res;
+    }
+};
