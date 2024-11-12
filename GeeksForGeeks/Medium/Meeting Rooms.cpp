d8a221b743468cf6cbe387229955f29fe22a50e5
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-12 21:28:05 +0530

    Create Meeting Rooms.cpp

---
+++ GeeksForGeeks/Medium/Meeting Rooms.cpp
@@ -0,0 +1,20 @@
+https://www.geeksforgeeks.org/problems/attend-all-meetings/1
+
+
+
+
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i][0]==arr[i+1][0]){
+                return false;
+            }
+            else if(arr[i][1]>arr[i+1][0]){
+                return false;
+            }
+        }
+        return 1;
+    }
+};
