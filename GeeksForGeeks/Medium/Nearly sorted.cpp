01e58555dcfc940f5e4f4fc1880bc3c28e21f753
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-14 16:04:42 +0530

    Create Nearly sorted.cpp

---
+++ GeeksForGeeks/Medium/Nearly sorted.cpp
@@ -0,0 +1,24 @@
+https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1
+
+
+
+
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        // code
+        int n=arr.size(), idx=0;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0; i<n; i++) {
+            pq.push(arr[i]);
+            if(pq.size()==k+1) {
+                arr[idx++]=pq.top();
+                pq.pop();
+            }
+        }
+        while(!pq.empty()) {
+            arr[idx++]=pq.top();
+            pq.pop();
+        }
+    }
+};
