422a5a718ff79d52556410aa7c4a23862f27191d
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-16 22:53:20 +0530

    Create Prodigy Student - Rectify code.js

---
+++ Codechef/Practice/Prodigy Student - Rectify code.js
@@ -0,0 +1,18 @@
+https://www.codechef.com/practice/course/javascript/LPJAS06/problems/LPJSPR65
+
+
+
+
+let name_1 = "Ajay";
+let name_2 = "Sumit";
+
+let marks_1 = 98.4;
+let marks_2 = 98.4;
+
+if (marks_2 < marks_1) {
+    console.log(name_2);
+} else if(marks_2 < marks_1){
+    console.log(name_1);
+} else{
+    console.log("equal");
+}
