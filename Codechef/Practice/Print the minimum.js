48935b0a12d5a5f137f58a737a1c10307ca69d89
Dhroov <dhroov1864.be20@chitkara.edu.in>
2024-11-18 23:59:48 +0530

    Create Print the minimum.js

---
+++ Codechef/Practice/Print the minimum.js
@@ -0,0 +1,22 @@
+https://codechef.com/practice/course/javascript/LPJAS06/problems/LPJSPR66
+
+
+
+
+let x = 2;
+let y = 3;
+let z = 1;
+
+if (x < y) {
+    if (x < z) {
+        console.log(x);
+    } else {
+        console.log(z);
+    }
+} else {
+    if (y < z) {
+        console.log(y);
+    } else {
+        console.log(x);
+    }
+}
